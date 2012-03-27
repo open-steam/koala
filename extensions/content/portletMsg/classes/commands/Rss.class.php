a298c33aaf272ca9aabaacc33e9a5c9f14cd7dc7
Marcel Jakoblew <mjako@upb.de>
2012-03-27 20:54:44 +0200

    portlet msg: rss feed works limited

--- extensions/content/portletMsg/classes/commands/Rss.class.php
+++ extensions/content/portletMsg/classes/commands/Rss.class.php
@@ -16,20 +16,14 @@ class Rss extends \AbstractCommand implements \IFrameCommand, \IIdCommand {
 	}
 	
 	public function processData(\IRequestObject $requestObject){
-                //$objectId=$requestObject->getId();
-		//$portletObject = \steam_factory::get_object( $GLOBALS["STEAM"]->get_id(), $objectId );
-		$params = $requestObject->getParams();
+                $params = $requestObject->getParams();
 		$objectId=$params[0];
 		$portlet = $portletObject = \steam_factory::get_object( $GLOBALS["STEAM"]->get_id(), $objectId );
 		
                 $steam = $GLOBALS["STEAM"];
                 
                 $steamUser = $steam->get_current_steam_user();
-                $config_webserver_ip = "";
-                
-                //var_dump($objectId);
-                //$object = $portletObject;
-                
+                $configWebserverIp = "";
                 
                 
 		//icon
@@ -43,9 +37,6 @@ class Rss extends \AbstractCommand implements \IFrameCommand, \IIdCommand {
 			$portletIsReference = false;
 		}
 		
-		
-		$this->getExtension()->addCSS();
-		$this->getExtension()->addJS();
 		
 		$portletName = $portlet->get_attribute(OBJ_DESC);
 		
@@ -66,15 +57,7 @@ class Rss extends \AbstractCommand implements \IFrameCommand, \IIdCommand {
 		$UBB = new \UBBCode();
 		include_once(PATH_BASE."core/lib/bid/derive_url.php");
 		
-		$portletFileName=$portletPath."/ui/html/index.html";
-		//$tmpl = new \HTML_TEMPLATE_IT();
-		//$tmpl->loadTemplateFile($portletFileName);
-		//$tmpl->setVariable("PORTLET_ID",$portlet->get_id());
 		
-                
-                //$htmlBody=$tmpl->get();
-		//$this->content=$htmlBody;
-                
 
                 if( !$steam || !$steam->get_login_status() ) {
                     echo("*** Login fehlgeschlagen! ***<br>");
@@ -83,60 +66,45 @@ class Rss extends \AbstractCommand implements \IFrameCommand, \IIdCommand {
  
                 
                 if ($portletObject->check_access_read($steamUser)) {
+                    $feedDescription = $portletObject->get_name();
                     
+                    //$feedLink = $configWebserverIp . $current_room->get_environment()->get_environment()->get_path();
+                    $feedLink = "localhost/PortletMsg/Rss/1520";
                     
-                    /*
-                    // Get room's attributes and store them
-                    if ($current_room->get_attribute("OBJ_DESC")){
-                        $feed_title = $current_room->get_attribute("OBJ_DESC");   
-                    }else{
-                        $feed_title = $current_room->get_name();
-                    }
-                    */
-                    
-                    $feed_description = $portletObject->get_name(); //TODO
-                    
-                    
-                    
-                    //$feed_link = $config_webserver_ip . $current_room->get_environment()->get_environment()->get_path();
-                    $feed_link = "localhost/PortletMsg/Rss/1520";
-                    
-                    
-
                     // Get inventory and store all relevant attributes in array entries
                     $inventory = $portletObject->get_inventory();
-                    $rss_items = array();
+                    $rssItems = array();
                     
                     //collect data for feed
                     foreach ($inventory as $item) {
                         if ($item->get_attribute("DOC_MIME_TYPE") == "text/plain") {
-                            $item_title = '<title>' . $item->get_name() . '</title>';
+                            $itemTitle = '<title>' . $item->get_name() . '</title>';
 
-                            $item_content = $item->get_content();
-                            $item_image = $item->get_attribute("bid:portlet:msg:picture_id");
+                            $itemContent = $item->get_content();
+                            $itemImage = $item->get_attribute("bid:portlet:msg:picture_id");
                             
-                            if ($item_image){
-                                $item_content = $item_content . '<div><img src="' . $config_webserver_ip . '/tools/get.php?object=' . $item_image . '" /></div>';
+                            if ($itemImage){
+                                $itemContent = $itemContent . '<div><img src="' . $configWebserverIp . '/tools/get.php?object=' . $itemImage . '" /></div>';
                             }
 
-                            $item_description = '<description><![CDATA[' . $item_content . ']]></description>';
+                            $itemDescription = '<description><![CDATA[' . $itemContent . ']]></description>';
 
-                            $item_link = $item->get_attribute("bid:portlet:msg:link_url");
-                            if ($item_link == ' '){
-                                $item_link = $feed_link;
+                            $itemLink = $item->get_attribute("bid:portlet:msg:link_url");
+                            if ($itemLink == ' '){
+                                $itemLink = $feedLink;
                             }
-                            $item_link = '<link>' . $feed_link . '</link>';
+                            $itemLink = '<link>' . $feedLink . '</link>';
 
                             $lastchanged = $item->get_attribute(DOC_LAST_MODIFIED);
                             if ($lastchanged === 0) {
                                 $lastchanged = $item->get_attribute(OBJ_CREATION_TIME);
                             }
-                            $item_pubDate = '<pubDate>' . strftime("%a, %d %b %Y %H:%M:%S GMT", $lastchanged) . '</pubDate>';
+                            $itemPubDate = '<pubDate>' . strftime("%a, %d %b %Y %H:%M:%S GMT", $lastchanged) . '</pubDate>';
 
                             // $author = '<author>' . $item->get_attribute("OBJ_OWNER") . '</author>';
-                            $item_guid = '<guid>' . $config_webserver_ip . '/index.php?object=' . $item->get_id() . '</guid>';
+                            $itemGuid = '<guid>' . $configWebserverIp . '/index.php?object=' . $item->get_id() . '</guid>';
 
-                            array_push ($rss_items, $item_title . $item_description . $item_link . $item_pubDate . $item_guid);
+                            array_push ($rssItems, $itemTitle . $itemDescription . $itemLink . $itemPubDate . $itemGuid);
                         }
                     }
 
@@ -152,19 +120,19 @@ class Rss extends \AbstractCommand implements \IFrameCommand, \IIdCommand {
                     echo "<?xml version='1.0' encoding='utf-8'?>\n";
                     echo "<rss version='2.0'>\n";
                     echo "<channel>\n";
-                    echo "<title>" . $feed_title . "</title>\n";
-                    echo "<description>" . $feed_description . "</description>\n";
-                    echo "<link>" . $feed_link . "</link>\n";
+                    echo "<title>" . $feedTitle . "</title>\n";
+                    echo "<description>" . $feedDescription . "</description>\n";
+                    echo "<link>" . $feedLink . "</link>\n";
                     echo "<generator>PHPsTeam/bid-owl 2.0</generator>\n";
                     echo "<ttl>60</ttl>\n";
 
-                    echo "<image><url>" . $config_webserver_ip . "/icons/bid_Logo_neu.gif</url>\n";
-                    echo "<title>" . $feed_title . "</title>\n";
-                    echo "<description>" . $feed_description . "</description>\n";
-                    echo "<link>" . $feed_link . "</link>\n";
+                    echo "<image><url>" . $configWebserverIp . "/icons/bid_Logo_neu.gif</url>\n";
+                    echo "<title>" . $feedTitle . "</title>\n";
+                    echo "<description>" . $feedDescription . "</description>\n";
+                    echo "<link>" . $feedLink . "</link>\n";
                     echo "</image>\n";
 
-                    foreach ($rss_items as $item){
+                    foreach ($rssItems as $item){
                         echo "<item>" . $item . "</item>\n";
                     }
                     
@@ -177,17 +145,7 @@ class Rss extends \AbstractCommand implements \IFrameCommand, \IIdCommand {
                     exit;
                 }
 
-                //Logout & Disconnect
-                //$steam->disconnect();
-
-                // end old stuff
-                
-                
-		//widgets
-		//$outputWidget = new \Widgets\RawHtml();
-		//$outputWidget->addWidget(new \Widgets\PopupMenu());
-		//$this->rawHtmlWidget = $outputWidget;
-	}
+        }
 	
 	public function idResponse(\IdResponseObject $idResponseObject) {
 		$idResponseObject->addWidget($this->rawHtmlWidget);
