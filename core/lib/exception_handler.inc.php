0a1715fe1dfc9133e9708b9424b18074d6013faa
Marcel Jakoblew <mjako@upb.de>
2012-04-02 13:37:43 +0200

    test

--- core/lib/exception_handler.inc.php
+++ core/lib/exception_handler.inc.php
@@ -3,7 +3,7 @@ include_once( PATH_LIB . 'encryption_handling.inc.php' );
 
 function send_http_error($pException, $pBacktrace = "", $silent = false) {
 	if ($pException->getCode() == E_USER_ACCESS_DENIED ) {
-		logging::write_log( LOG_403, date("d.m.Y H:i", time()) . " USER: " . $_ENV["USER"] . " " . "HTTP-" . $_SERVER[ 'REQUEST_METHOD' ]. ': ' . $_SERVER[ 'REQUEST_URI' ] . "\n" . $pException->getMessage());
+		logging::write_log( LOG_403, date("d.m.Y H:i", time()) . " USER: " . $_ENV["USER"] . " " . "HTTP-" . $_SERVER[ 'REQUEST_METHOD' ]. ': ' . $_SERVER[ 'REQUEST_URI' ] . "\n" . $pException->getTraceAsString());
                 die;
                 header( 'Location: ' . PATH_URL . "403/");
 		exit;
