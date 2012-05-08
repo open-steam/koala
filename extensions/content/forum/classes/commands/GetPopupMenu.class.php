c4956378747b05859d6d4dbda10a03a36ea0ba15
Christoph Sens <csens@mail.uni-paderborn.de>
2012-05-08 17:02:08 +0200

    *forum: changed icons

--- extensions/content/forum/classes/commands/GetPopupMenu.class.php
+++ extensions/content/forum/classes/commands/GetPopupMenu.class.php
@@ -78,7 +78,8 @@ class GetPopupMenu extends \AbstractCommand implements \IAjaxCommand {
 		$propertiesIcon = \Explorer::getInstance()->getAssetUrl() . "icons/menu/properties.png";
 		$rightsIcon = \Explorer::getInstance()->getAssetUrl() . "icons/menu/rights.png";
 		$blankIcon = \Explorer::getInstance()->getAssetUrl() . "icons/menu/blank.png";
-		$replyIcon = "";
+		$replyIcon = \Explorer::getInstance()->getAssetUrl() . "icons/menu/reply.png";
+                $addImageIcon= \Explorer::getInstance()->getAssetUrl() . "icons/menu/add_image.png";
 		$deleteIcon=\Explorer::getInstance()->getAssetUrl() . "icons/menu/delete.png";
 
 		$steamUser= \lms_steam::get_current_user();
@@ -134,7 +135,7 @@ class GetPopupMenu extends \AbstractCommand implements \IAjaxCommand {
 			if($isTopicCreator){
 				$items[]=array("name" => "Bearbeiten<img src=\"{$editIcon}\">", "command" => "EditTopic", "namespace" => "forum", "params" => "{'id':'{$this->id}','forum':'{$forumId}'}", "type" => "popup");
 				$items[]=array("name" => "Löschen<img src=\"{$trashIcon}\">", "command" => "DeleteTopic", "namespace" => "forum", "params" => "{'id':'{$this->id}','forum':'{$forumId}'}");
-				$items[]= array("name" => "Bild anfügen<img src=\"{$editIcon}\">", "command" => "EditMessageImage", "namespace" => "forum", "params" => "{'messageObjectId':'{$this->id}','forum':'{$forumId}'}", "type" => "popup");
+				$items[]= array("name" => "Bild anfügen<img src=\"{$addImageIcon}\">", "command" => "EditMessageImage", "namespace" => "forum", "params" => "{'messageObjectId':'{$this->id}','forum':'{$forumId}'}", "type" => "popup");
 			}
 		}else{
 			if($canAnnotate){
@@ -145,7 +146,7 @@ class GetPopupMenu extends \AbstractCommand implements \IAjaxCommand {
 				$items[]=array("name" => "Bearbeiten<img src=\"{$editIcon}\">", "command" => "EditReply", "namespace" => "forum", "params" => "{'id':'{$this->id}','forum':'{$forumId}'}", "type" => "popup");
 				$items[]=array("name" => "Löschen<img src=\"{$deleteIcon}\">", "command" => "DeleteReply", "namespace" => "forum", "params" => "{'id':'{$this->id}','forum':'{$forumId}'}");
 				}
-				$items[]= array("name" => "Bild anfügen<img src=\"{$editIcon}\">", "command" => "EditMessageImage", "namespace" => "forum", "params" => "{'messageObjectId':'{$this->id}','forum':'{$forumId}'}", "type" => "popup");
+				$items[]= array("name" => "Bild anfügen<img src=\"{$addImageIcon}\">", "command" => "EditMessageImage", "namespace" => "forum", "params" => "{'messageObjectId':'{$this->id}','forum':'{$forumId}'}", "type" => "popup");
 							
 			}
 		}
