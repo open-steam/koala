98b79151b8edfac4fb0afe0c0238978e1d2843b7
Marcel Jakoblew <mjako@upb.de>
2012-08-01 02:11:11 +0200

    added portal sortable test

--- extensions/content/portal/ui/js/code.js
+++ extensions/content/portal/ui/js/code.js
@@ -10,3 +10,21 @@ function portalLockButton(id){
 		}
 	}
 }
+
+//jquery sortable code
+$(function() {
+        $(".column").sortable({
+                connectWith: ".column"
+        });
+        
+        /*
+        $(".portlet").addClass("ui-widget ui-widget-content ui-helper-clearfix ui-corner-all")
+                .find(".portlet-header")
+                .addClass("ui-widget-header ui-corner-all")
+                .prepend("<span class='ui-icon ui-icon-minusthick'></span>")
+                .end()
+                .find(".portlet-content");
+        */
+        
+        $(".column").disableSelection();
+});
