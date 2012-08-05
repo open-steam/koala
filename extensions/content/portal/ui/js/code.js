fa0c8b402e68d9bde13a4ac22b3ed82e0463f4f4
Marcel Jakoblew <mjako@upb.de>
2012-08-05 16:29:53 +0200

    added comment for sorting

--- extensions/content/portal/ui/js/code.js
+++ extensions/content/portal/ui/js/code.js
@@ -18,6 +18,13 @@ $(function() {
                 items:".portlet",
                 placeholder: "ui-state-highlight",
                 update: function(event, ui) { alert("jetzt sollte persistiert werden..."); }
+                //idee: 
+                //per javascript nach der reihenfolge der portlets suchten
+                //start bei div mit class=portal
+                //darunter folgen die 3 spalten
+                //und in jeder spalte sind die portlets
+                //die div-id eines portlets in der oberfläche ist die objekt-id des objektes
+                //aus der mit der ermittelten reihenfolge einen (noch fehlenden) php-command aufrufen, der die sortierung durchführt
                 
         });
         
