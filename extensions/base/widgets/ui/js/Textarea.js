97400eeecf0e4fd48204c2bb0476c6c43ee203cd
Marcel Jakoblew <mjako@upb.de>
2012-08-08 00:34:33 +0200

    test path change asciisvg

--- extensions/base/widgets/ui/js/Textarea.js
+++ extensions/base/widgets/ui/js/Textarea.js
@@ -128,10 +128,12 @@ function widgets_textarea_save_success(elementId, response) {
 						fix_table_elements : true,
                                                 readonly : tinymceReadOnly,
                                                 verify_html : false,
-
+                                                
+                                                //test cases
                                                 //AScgiloc : 'http://www.imathas.com/imathas/filter/graph/svgimg.php', //orginalquelle
                                                 AScgiloc : 'http://www.bid-owl.de/tools/asciisvg/svgimg.php', //aus bid2
                                                 //AScgiloc : 'https://www.lspb.de/tools/asciisvg/svgimg.php',
+                                                //AScgiloc : '/tools/asciisvg/svgimg.php',
                                                 
 						ASdloc : pathUrl + '/styles/standard/javascript/tinymce-jquery/jscripts/tiny_mce/plugins/asciisvg/js/d.svg',
 						fullscreen_new_window : true,       
