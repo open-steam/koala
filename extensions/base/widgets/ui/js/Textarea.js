5707c01154b712881ae50c860818476cc8bf5cff
Marcel Jakoblew <mjako@upb.de>
2012-08-19 17:28:04 +0200

    html text: formula, graph modul

--- extensions/base/widgets/ui/js/Textarea.js
+++ extensions/base/widgets/ui/js/Textarea.js
@@ -131,8 +131,8 @@ function widgets_textarea_save_success(elementId, response) {
                                                 
                                                 //test cases
                                                 //AScgiloc : 'http://www.imathas.com/imathas/filter/graph/svgimg.php', //orginalquelle
-                                                //AScgiloc : 'http://www.bid-owl.de/tools/asciisvg/svgimg.php', //aus bid2
-                                                AScgiloc : 'http://www.lspb.de/tools/asciisvg/svgimg.php',
+                                                //AScgiloc : 'http://www.bid-owl.de/tools/asciisvg/svgimg.php', //aus bid2, läuft
+                                                AScgiloc : 'https://steam.lspb.de/tools/asciisvg/svgimg.php', //??
                                                 //AScgiloc : '/tools/asciisvg/svgimg.php',
                                                 
 						ASdloc : pathUrl + '/styles/standard/javascript/tinymce-jquery/jscripts/tiny_mce/plugins/asciisvg/js/d.svg',
