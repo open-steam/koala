4e6ba792077b1511e2af5135d668d342415a5c33
Christoph Sens <csens@mail.uni-paderborn.de>
2012-09-10 21:31:06 +0200

    *added dialog to create an object

--- extensions/content/portletTermplan/classes/commands/Create.class.php
+++ extensions/content/portletTermplan/classes/commands/Create.class.php
@@ -1,89 +1,96 @@
 <?php
+
 namespace PortletTermplan\Commands;
+
 class Create extends \AbstractCommand implements \IFrameCommand, \IIdCommand, \IAjaxCommand {
-	
-	private $params;
-	private $id;
-	private $content;
-	private $rawHtmlWidget;
-	
-	public function validateData(\IRequestObject $requestObject) {
-		return true;
-	}
-	
-	public function processData(\IRequestObject $requestObject){
-		$params = $requestObject->getParams();
-		
-		$name = $params["title"];
-		$column = $params["parent"];
-		$version = "1.0";		
-		
-		//check diffrent types of parameter
-		if(is_string($column)){
-			$columnObject =  \steam_factory::get_object($GLOBALS["STEAM"]->get_id(),$column);
-		}else{
-			$columnObject = $column;
-		}
-		
-		
-		//get date
-		$currentYear = date("Y")."";
-		$nextYear = (date("Y")+1)."";
-		
-		//create
-		$termPlanObject = \steam_factory::create_container($GLOBALS["STEAM"]->get_id(), $name, $columnObject);
-	    
-		$pollTopic = "Beschreibung";
-	    $startDate = array("day" => "01","month" => "01","year" => $currentYear,);
-	    $endDate = array("day" => "01","month" => "01","year" => $nextYear,);
-	    $options = array("Termin A","Termin B","Termin C","Termin D","Termin E","Termin F");
-	    $optionsVotecount = array(0,0,0,0,0,0);
-	    
-	    $termPlanContent = array(	"end_date" => $endDate,
-	    						"options" => $options,
-	    						"options_votecount" => $optionsVotecount,
-	    						"poll_topic" => $pollTopic,
-	    						"start_date" => $startDate,
-	    );
-	    
-	    $termPlanObject->set_attributes(array(
-	        OBJ_DESC => $name,
-	        OBJ_TYPE => "container_portlet_bid",
-	        "bid:portlet" => "termplan",
-	        "bid:portlet:version" => $version,
-	        "bid:portlet:content" => $termPlanContent,
-	    ));
-	    
-	    $termChoices = "0"; //initial value
-	    $termPlanObject->set_attribute("termChoices", $termChoices);
-	    
-	    //sanctions
-	    $everybody = \steam_factory::get_group( $GLOBALS["STEAM"]->get_id(), "steam" );
-		$termPlanObject->set_sanction($everybody, SANCTION_READ | SANCTION_WRITE );
-	
-		
-	}
-	
-	public function idResponse(\IdResponseObject $idResponseObject) {
-		
-	}
 
-	
-	public function frameResponse(\FrameResponseObject $frameResponseObject) {
-		
-	}
+    private $params;
+    private $id;
+    private $content;
+    private $rawHtmlWidget;
 
-	
-	public function ajaxResponse(\AjaxResponseObject $ajaxResponseObject) {
-		$ajaxResponseObject->setStatus("ok");
-		$jswrapper = new \Widgets\JSWrapper();
-		$jswrapper->setJs(<<<END
+    public function validateData(\IRequestObject $requestObject) {
+        return true;
+    }
+
+    public function processData(\IRequestObject $requestObject) {
+        $params = $requestObject->getParams();
+        $name = $params["title"];
+        $column = $params["id"];
+        $version = "1.0";
+        $desc = $params["desc"];
+
+        $startD = $params["startDate"];
+        $endD = $params["endDate"];
+        $startDateArray = array();
+        $startDateArray = explode(".", $startD);
+        $endDateArray = array();
+        $endDateArray = explode(".", $endD);
+
+        $term0 = $params["term0"];
+        $term1 = $params["term1"];
+        $term2 = $params["term2"];
+        $term3 = $params["term3"];
+        $term4 = $params["term4"];
+        $term5 = $params["term5"];
+        //check diffrent types of parameter
+        if (is_string($column)) {
+            $columnObject = \steam_factory::get_object($GLOBALS["STEAM"]->get_id(), $column);
+        } else {
+            $columnObject = $column;
+        }
+
+        //create
+        $termPlanObject = \steam_factory::create_container($GLOBALS["STEAM"]->get_id(), $name, $columnObject);
+
+        $pollTopic = $desc;
+        $startDate = array("day" => $startDateArray[0], "month" => $startDateArray[1], "year" => $startDateArray[2],);
+        $endDate = array("day" => $endDateArray[0], "month" => $endDateArray[1], "year" => $endDateArray[2],);
+        $options = array($term0, $term1, $term2, $term3, $term4, $term5);
+        $optionsVotecount = array(0, 0, 0, 0, 0, 0);
+
+        $termPlanContent = array("end_date" => $endDate,
+            "options" => $options,
+            "options_votecount" => $optionsVotecount,
+            "poll_topic" => $pollTopic,
+            "start_date" => $startDate,
+        );
+
+        $termPlanObject->set_attributes(array(
+            OBJ_DESC => $name,
+            OBJ_TYPE => "container_portlet_bid",
+            "bid:portlet" => "termplan",
+            "bid:portlet:version" => $version,
+            "bid:portlet:content" => $termPlanContent,
+        ));
+
+        $termChoices = "0"; //initial value
+        $termPlanObject->set_attribute("termChoices", $termChoices);
+
+        //sanctions
+        $everybody = \steam_factory::get_group($GLOBALS["STEAM"]->get_id(), "steam");
+        $termPlanObject->set_sanction($everybody, SANCTION_READ | SANCTION_WRITE);
+    }
+
+    public function idResponse(\IdResponseObject $idResponseObject) {
+        
+    }
+
+    public function frameResponse(\FrameResponseObject $frameResponseObject) {
+        
+    }
+
+    public function ajaxResponse(\AjaxResponseObject $ajaxResponseObject) {
+        $ajaxResponseObject->setStatus("ok");
+        $jswrapper = new \Widgets\JSWrapper();
+        $jswrapper->setJs(<<<END
 		window.location.reload();
 END
-		);
-		$ajaxResponseObject->addWidget($jswrapper);
-		return $ajaxResponseObject;
-	}
-	
+        );
+        $ajaxResponseObject->addWidget($jswrapper);
+        return $ajaxResponseObject;
+    }
+
 }
+
 ?>
--- extensions/content/portletTermplan/classes/commands/CreateNewForm.class.php
+++ extensions/content/portletTermplan/classes/commands/CreateNewForm.class.php
@@ -1,90 +1,123 @@
 <?php
+
 namespace PortletTermplan\Commands;
-class CreateNewForm extends \AbstractCommand implements \IFrameCommand, \IIdCommand, \IAjaxCommand {
-	
-	private $params;
-	private $id;
-	private $content;
-	private $dialog;
-	
-	public function validateData(\IRequestObject $requestObject) {
-		return true;
-	}
-	
-	public function processData(\IRequestObject $requestObject){
-		$this->params = $requestObject->getParams();
-		$this->id = $this->params["id"];
-	}
-	
-	public function idResponse(\IdResponseObject $idResponseObject) {
-		
-	}
-	
-	public function frameResponse(\FrameResponseObject $frameResponseObject) {
-		
-	}
-	
-	public function ajaxResponse(\AjaxResponseObject $ajaxResponseObject) {
-		$ajaxResponseObject->setStatus("ok");
-		
-		$ajaxForm = new \Widgets\AjaxForm();
-		$ajaxForm->setSubmitCommand("Create");
-		$ajaxForm->setSubmitNamespace("PortletTermplan");
 
-		$ajaxForm->setHtml(<<<END
-<style type="text/css">
-.attribute {
-  clear: left;
-  padding: 5px 2px 5px 2px;
+class CreateNewForm extends \AbstractCommand implements \IAjaxCommand {
+
+    private $params;
+    private $id;
+    private $content;
+    private $dialog;
+
+    public function validateData(\IRequestObject $requestObject) {
+        return true;
+    }
+
+    public function processData(\IRequestObject $requestObject) {
+        $this->params = $requestObject->getParams();
+        $this->id = $this->params["id"];
+    }
+
+    public function ajaxResponse(\AjaxResponseObject $ajaxResponseObject) {
+
+        $ajaxResponseObject->setStatus("ok");
+
+        $ajaxForm = new \Widgets\AjaxForm();
+        $ajaxForm->setSubmitCommand("Create");
+        $ajaxForm->setSubmitNamespace("PortletTermplan");
+
+        $currentDay = date("d") . "";
+        $currentMonth = date("m") . "";
+        $currentYear = date("Y") . "";
+        if (intval($currentMonth) <= 11) {
+            $futureMonth = intval($currentMonth) + 1;
+            $futureYear = intval($currentYear);
+        } else {
+            $futureMonth = 1;
+            $futureYear = intval($currentYear) + 1;
+        }
+
+        $currentDate = $currentDay . "." . $currentMonth . "." . $currentYear;
+        $futureDate = $currentDay . "." . $futureMonth . "." . $futureYear;
+
+        $titelInput = new \Widgets\TextInput();
+        $titelInput->setLabel("Überschrift");
+        $titelInput->setName("title");
+        $html = $titelInput->getHtml();
+
+        $descriptionInput = new \Widgets\TextInput();
+        $descriptionInput->setLabel("Beschreibung");
+        $descriptionInput->setName("desc");
+        $html .= $descriptionInput->getHtml();
+
+        $datepickerStart = new \Widgets\DatePicker();
+        $datepickerStart->setLabel("Start der Abstimmung");
+        $datepickerStart->setName("startDate");
+        $html .= $datepickerStart->getHtml();
+        $html .='<script>$("input[name=\"startDate\"]").val("' . $currentDate . '");</script>';
+
+        $datepickerEnd = new \Widgets\DatePicker();
+        $datepickerEnd->setLabel("Ende der Abstimmung");
+        $datepickerEnd->setName("endDate");
+        $html .= $datepickerEnd->getHtml();
+        $html .='<script>$("input[name=\"endDate\"]").val("' . $futureDate . '");</script>';
+
+
+        $termLabelWidth = 50;
+        $termInputWidth = 300;
+
+        $term0 = new \Widgets\TextInput();
+        $term0->setLabelWidth($termLabelWidth);
+        $term0->setInputWidth($termInputWidth);
+        $term0->setLabel("Eintrag 1");
+        $term0->setName("term0");
+        $html .= $term0->getHtml();
+        $html .='<script>$("input[name=\"term0\"]").val("Termin A");</script>';
+
+        $term1 = new \Widgets\TextInput();
+        $term1->setLabelWidth($termLabelWidth);
+        $term1->setInputWidth($termInputWidth);
+        $term1->setLabel("Eintrag 2");
+        $term1->setName("term1");
+        $html .= $term1->getHtml();
+        $html .='<script>$("input[name=\"term1\"]").val("Termin B");</script>';
+
+
+        $term2 = new \Widgets\TextInput();
+        $term2->setLabelWidth($termLabelWidth);
+        $term2->setInputWidth($termInputWidth);
+        $term2->setLabel("Eintrag 3");
+        $term2->setName("term2");
+        $html .= $term2->getHtml();
+
+        $term3 = new \Widgets\TextInput();
+        $term3->setLabelWidth($termLabelWidth);
+        $term3->setInputWidth($termInputWidth);
+        $term3->setLabel("Eintrag 4");
+        $term3->setName("term3");
+        $html .= $term3->getHtml();
+
+        $term4 = new \Widgets\TextInput();
+        $term4->setLabelWidth($termLabelWidth);
+        $term4->setInputWidth($termInputWidth);
+        $term4->setLabel("Eintrag 5");
+        $term4->setName("term4");
+        $html .= $term4->getHtml();
+
+        $term5 = new \Widgets\TextInput();
+        $term5->setLabelWidth($termLabelWidth);
+        $term5->setInputWidth($termInputWidth);
+        $term5->setLabel("Eintrag 6");
+        $term5->setName("term5");
+        $html .= $term5->getHtml();
+        
+        $html .= '<input type="hidden" name="id" value="'.$this->id.'">';
+
+        $ajaxForm->setHtml($html);
+        $ajaxResponseObject->addWidget($ajaxForm);
+        return $ajaxResponseObject;
+    }
+
 }
 
-.attributeName {
-  float: left;
-  padding-right: 20px;
-  text-align: right;
-  width: 80px;
-}
-
-.attributeNameRequired {
-  float: left;
-  padding-right: 20px;
-  text-align: right;
-  font-weight: bold;
-  width: 80px;
-}
-
-.attributeValue {
-  float: left;
-  width: 300px;
-}
-
-.attributeValue .text, .attributeValue textarea {
-  wwidth: 100px;
-}
-
-.attributeValueColumn {
-  float: left;
-  position: relative;
-  text-align: center;
-}
-</style>
-<input type="hidden" name="id" value="{$this->id}">
-
-<div class="attribute">
-	<div class="attributeNameRequired">Titel*:</div>
-	<div><input type="text" class="text" value="" name="title"></div>
-</div>
-
-<div class="attribute">
-	<div><input type="hidden" name="parent" value="{$this->id}"></div>
-</div>
-
-
-
-END
-);
-		$ajaxResponseObject->addWidget($ajaxForm);
-		return $ajaxResponseObject;
-	}
-}
 ?>
