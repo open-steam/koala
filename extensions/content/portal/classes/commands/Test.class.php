003ee516422fa9198d6d72bef2919973ad81d434
Christoph Sens <csens@mail.uni-paderborn.de>
2012-11-25 16:17:35 +0100

    *http post request

--- extensions/content/portal/classes/commands/Test.class.php
+++ extensions/content/portal/classes/commands/Test.class.php
@@ -2,9 +2,11 @@
 
 namespace Portal\Commands;
 
-class Test extends \AbstractCommand implements \IFrameCommand {
+require_once 'ical/When.php';
+require_once 'ical/When_Iterator.php';
+require_once 'ical/reader.php';
 
-    
+class Test extends \AbstractCommand implements \IFrameCommand {
 
     public function validateData(\IRequestObject $requestObject) {
         return true;
@@ -16,17 +18,38 @@ class Test extends \AbstractCommand implements \IFrameCommand {
     }
 
     public function frameResponse(\FrameResponseObject $frameResponseObject) {
-        $pgb = new \Widgets\Progressbar();
-        $pgb->setId("2");
-        $pgb->setValue(50);
-        $pgb->setWidth("200px");
-        $widget = new \Widgets\Slider();
-        $widget->setId("1");
-        $widget->setMax(20);
-        $rawHtml = new \Widgets\RawHtml();
-        $rawHtml->setHtml($pgb->getHtml());
-        $frameResponseObject->addWidget($rawHtml);
+
+
+        $data_to_send = '<?xml version="1.0" encoding="utf-8" ?>
+<C:free-busy-query xmlns:C="urn:ietf:params:xml:ns:caldav">
+<C:time-range start="20060104T140000Z" end="20060105T220000Z"/>
+</C:free-busy-query>';
+        $fp = fsockopen("www.google.com", 80);
+        if (!$fp) {
+            echo "Keine Verbindung möglich!";
+        } else {
+            echo "Verbindung hergestellt!";
+            fputs($fp, "POST /calendar/dav/christoph.sens@gmail.com/user HTTP/1.1\r\n");
+            fputs($fp, "Host: www.google.com\r\n");
+            fputs($fp, "Content-type: application/x-www-form-urlencoded\r\n");
+            fputs($fp, "Content-length: " . strlen($data_to_send) . "\r\n");
+            fputs($fp, "Connection: close\r\n\r\n");
+            fputs($fp, $data_to_send);
+            $res = "";
+            while (!feof($fp)) {
+                $res .= fgets($fp, 128);
+            }
+            printf("Done!\n");
+            fclose($fp);
+
+            echo $res;
+        }
+        die;
+
+
+
         return $frameResponseObject;
-        
     }
+
 }
+
