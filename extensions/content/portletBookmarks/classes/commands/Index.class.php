897ddf11d9421556dbf8b91a936502b6987974a7
Christoph Sens <csens@mail.uni-paderborn.de>
2012-12-08 20:42:17 +0100

    *added

--- extensions/content/portletBookmarks/classes/commands/Index.class.php
+++ extensions/content/portletBookmarks/classes/commands/Index.class.php
@@ -2,7 +2,7 @@
 
 namespace PortletBookmarks\Commands;
 
-class Index extends \AbstractCommand implements \IFrameCommand, \IIdCommand {
+class Index extends \AbstractCommand implements \IIdCommand {
 
     private $params;
     private $id;
@@ -26,24 +26,120 @@ class Index extends \AbstractCommand implements \IFrameCommand, \IIdCommand {
         if (count($bookmarks) <= $numberOfBookmarks) {
             $n = count($bookmarks);
         }
+        $viewBookmarks = array();
         for ($i = 0; $i < $n; $i++) {
-            //Erzeuge eine grafische Ausgabe!!!!
+            $viewBookmarks[$i] = $bookmarks[$i];
         }
-        
+        $listViewer = new \Widgets\ListViewer();
+        $listViewer->setHeadlineProvider(new HeadlineProvider());
+        $listViewer->setContentProvider(new ContentProvider());
+        $listViewer->setColorProvider(new ColorProvider());
+        $listViewer->setContentFilter(new ContentFilter());
+        $listViewer->setContent($viewBookmarks);
+        $rawHtml = new \Widgets\RawHtml();
+        $rawHtml->addWidget($listViewer);
+        $rawHtml->setHtml($listViewer->getHtml() . "<br><a class=\"pill button\" href=\"" . PATH_URL . "bookmarks/\">Alle Lesezeichen anzeigen</a>");
+
+        $this->content = $rawHtml;
     }
 
     public function idResponse(\IdResponseObject $idResponseObject) {
-        $rawHtml = new \Widgets\RawHtml();
-        $rawHtml->setHtml("Hallo");
-        $idResponseObject->addWidget($rawHtml);
+        $idResponseObject->addWidget($this->content);
         return $idResponseObject;
     }
 
     public function frameResponse(\FrameResponseObject $frameResponseObject) {
-       
-        
-        
+
+
+
         return $frameResponseObject;
+    }
+
+}
+
+class HeadlineProvider implements \Widgets\IHeadlineProvider {
+
+    public function getHeadlines() {
+        //return array("", "Name", "Marker","Änderungsdatum", "Größe");
+        return array("", "", "");
+    }
+
+    public function getHeadLineWidths() {
+        return array(22, 580, 150);
+        //return array(22, 200, 130);
+    }
+
+    public function getHeadLineAligns() {
+        return array("left", "left", "right");
+    }
+
+}
+
+class ContentProvider implements \Widgets\IContentProvider {
+
+    private $rawImage = 0;
+    private $rawName = 1;
+    private $rawChangeDate = 2;
+
+    public function getId($contentItem) {
+        return $contentItem->get_id();
+    }
+
+    public function getCellData($cell, $contentItem) {
+        if (!is_int($cell)) {
+            throw new \Exception("cell must be an integer!!");
+        }
+
+        if ($cell == $this->rawImage) {
+            return "<img src=\"" . PATH_URL . "explorer/asset/icons/mimetype/" . deriveIcon($contentItem->get_source_object()) . "\"></img>";
+        } else if ($cell == $this->rawName) {
+            $url = \ExtensionMaster::getInstance()->getUrlForObjectId($contentItem->get_source_object()->get_id(), "view");
+            $desc = $contentItem->get_source_object()->get_attribute("OBJ_DESC");
+            $name = getCleanName($contentItem, 50);
+
+            //check existence of link target
+            $sourceObject = $contentItem->get_link_object();
+            if (!(($sourceObject != null) && ($sourceObject instanceof \steam_object))) {
+                return "<div style=\"color:red\">$name (Lesezeichenziel gelöscht)</div>";
+            }
+
+            if (isset($url) && $url != "") {
+                return "<a href=\"" . $url . "\" title=\"$desc\"> " . $name . "</a>";
+            } else {
+                return $name;
+            }
+        } else if ($cell == $this->rawChangeDate) {
+            return getReadableDate($contentItem->get_source_object()->get_attribute("OBJ_LAST_CHANGED"));
+        }
+    }
+
+    public function getNoContentText() {
+        return "Hier können Sie Lesezeichen zu beliebigen Ordnern und Dokumenten anlegen. Wenn Sie den Inhalt eines Ordners betrachten, können Sie Lesezeichen anlegen.";
+    }
+
+    public function getOnClickHandler($contentItem) {
+        return "";
+    }
+
+}
+
+class ColorProvider implements \Widgets\IColorProvider {
+
+    public function getColor($contentItem) {
+        $color = $contentItem->get_attribute("OBJ_COLOR_LABEL");
+        return ($color === 0) ? "" : $color;
+    }
+
+}
+
+class ContentFilter implements \Widgets\IContentFilter {
+
+    public function filterObject($object) {
+        if ($object instanceof \steam_user) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
