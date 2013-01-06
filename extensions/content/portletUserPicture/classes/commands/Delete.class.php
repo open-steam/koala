3754eb4c37745e986181be84a19bc5fd7b6f0397
Jan Petertonkoker <petertonkoker@gmail.com>
2013-01-06 16:16:35 +0100

    home portal update and filetree bugfix

---
+++ extensions/content/portletUserPicture/classes/commands/Delete.class.php
@@ -0,0 +1,29 @@
+<?php
+namespace PortletUserPicture\Commands;
+class Delete extends \AbstractCommand implements \IAjaxCommand {
+	
+	public function validateData(\IRequestObject $requestObject) {
+		return true;
+	}
+	
+	public function processData(\IRequestObject $requestObject){
+		$params = $requestObject->getParams();
+		$steamObject = \steam_factory::get_object($GLOBALS["STEAM"]->get_id(), $params["portletId"]);
+		
+		//delete the object
+		$trashbin = $GLOBALS["STEAM"]->get_current_steam_user()->get_attribute("USER_TRASHBIN");
+		$steamObject->move($trashbin);
+	}
+	
+	public function ajaxResponse(\AjaxResponseObject $ajaxResponseObject) {
+		$ajaxResponseObject->setStatus("ok");
+		$jswrapper = new \Widgets\JSWrapper();
+		$jswrapper->setJs(<<<END
+		window.location.reload();
+END
+		);
+		$ajaxResponseObject->addWidget($jswrapper);
+		return $ajaxResponseObject;
+	}
+}
+?>
