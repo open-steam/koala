a3e3867464de190d31628f8de509c7cabda8385d
Christoph Sens <csens@mail.uni-paderborn.de>
2013-10-03 19:23:25 +0200

    *added picture upload

---
+++ extensions/content/rapidfeedback/classes/commands/DeleteImage.class.php
@@ -0,0 +1,38 @@
+<?php
+namespace Rapidfeedback\Commands;
+class DeleteImage extends \AbstractCommand implements \IAjaxCommand {
+	
+	private $params;
+	private $id;
+	
+	public function validateData(\IRequestObject $requestObject) {
+		return true;
+	}
+	
+	public function processData(\IRequestObject $requestObject) {
+		if ($requestObject instanceof \UrlRequestObject) {
+			$this->params = $requestObject->getParams();
+			isset($this->params[0]) ? $this->id = $this->params[0]: "";
+		} else if ($requestObject instanceof \AjaxRequestObject) {
+			$this->params = $requestObject->getParams();
+			isset($this->params["id"]) ? $this->id = $this->params["id"]: "";
+		}
+	}
+	
+	public function ajaxResponse(\AjaxResponseObject $ajaxResponseObject) {
+            	$destObject = \steam_factory::get_object($GLOBALS["STEAM"]->get_id(), $this->id);
+		//remove old image
+		$oldImageId = $destObject->get_attribute("bid:rfb:picture_id");
+		if ($oldImageId !== 0) {
+			$oldImage = \steam_factory::get_object($GLOBALS["STEAM"]->get_id(), $oldImageId);
+			if ($oldImage instanceof \steam_document) {
+				$destObject->delete_attribute("bid:rfb:picture_id");
+				$oldImage ->delete();
+			}
+		}
+		$ajaxResponseObject->setStatus("ok");
+		return $ajaxResponseObject;
+	}
+}
+
+?>
