9a03f433bd013160a34d8d2e049b23e9eacfb3be
Dominik Niehus <dominik.niehus@coactum.de>
2013-11-07 15:39:40 +0100

    adding platform schulen-gt

---
+++ platforms/schulen-gt/extensions/info/ge/Philosophie.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"><html xmlns="http://www.w3.org/1999/xhtml" xml:lang="de" lang="de"><head><meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+<link href="../../css/static_pages.css" rel="stylesheet" type="text/css" />
+<?php include ("../../../config/config.php"); ?>
+<title>Philosophie</title>
+</head>
+
+<body>
+
+<p><a href="<?php echo $config_webserver_ip ?>"><img class="logo" src="../../../icons/logo.gif" alt ="<?php echo $config_server_name ?>" /></a></p>
+
+<h1>Philosophie</h1>
+
+<p>Der Server <i><?php echo $config_server_name ?></i> ist in erster Linie ein Kooperationsforum. Jeder Projektgruppe wird ein in sich abgeschlossener Arbeitsraum zur Verfügung gestellt, in dem ungestört und unbeobachtet kommuniziert und gearbeitet werden kann. Eine einfache, benutzerorientierte Verwaltung organisiert die Gruppenzugehörigkeit und die Rechtevergabe für jedes Dokument. Alle gängigen Dateiformate lassen sich einbinden.</p>
+
+<p>Einerseits ist der Arbeitsbereich sehr ähnlich der bekannten Explorer-Oberfläche in MS Windows gestaltet. Man kann auch weiterhin mit der gewohnten Software seine Dokumente erstellen und in bekannter Weise organisieren. Andererseits bietet <i><?php echo $config_server_name ?></i> mit seinen Portalseiten einen einfachen Weg, Ergebnisse auf eine ansprechende Weise zugänglich zu machen. Die Präsentation fertiger Ergebnisse geschieht durch wenige Mausklicks.</p>
+
+<h2>An wen richtet sich das Angebot?</h2>
+
+<p><i><?php echo $config_server_name ?></i> lebt von Ihrem Engagement. Alle, die sich dem Thema Bildung verbunden fühlen, sind eingeladen:</p>
+
+<ul>
+<li>eine ganze Schule</li>
+<li>eine Klasse</li>
+<li>ein Kurs</li>
+<li>eine AG</li>
+<li>eine Schülerzeitung</li>
+<li>eine Lehrerarbeitsgemeinschaft</li>
+<li>Ihre Projektgruppe...</li>
+</ul>
+
+<h2>Arbeiten mit <i><?php echo $config_server_name ?></i></h2>
+
+<p><i><?php echo $config_server_name ?></i> ist eine moderne Arbeits- und Präsentationsplattform, die allen zur Verfügung steht, die sich in der Bildungslandschaft Ostwestfalen-Lippe engagieren. Die Etablierung einer benutzer- und gruppenspezifischen Arbeitsumgebung ermöglicht ein »Wissensmanagement im Inter- und Intranet« ohne den Aufbau und die Pflege eines eigenen Servers. Darüber hinaus wird schulübergreifendes kooperatives Lernen unterstützt und deutlich erleichtert. In fachorientierten Arbeitsgruppen können Schüler und Lehrer verschiedener Schulen Erfahrungen und Unterrichtsanregungen austauschen.</p>
+
+<h2>Wie kann ich mich anmelden?</h2>
+
+<p>Für weitere Informationen, oder besser noch – zum Einrichten einer Gruppe und eines persönlichen Zugangs – stehen wir Ihnen gerne zur Verfügung: Schicken Sie uns eine E-Mail mit Ihrem Namen, Vornamen, Institution (Schule etc.) und Ihrer E-Mail-Adresse. Sie erhalten dann umgehend Ihre Zugangsdaten (Benutzername, Kennwort) zugesandt. Bei Bedarf organisieren wir Schulungen zu Einsatzmöglichkeiten und zur speziellen Benutzung des Servers.</p>
+
+<h2><a href="Ansprechpartner.php">Kooperationspartner und Kontaktadressen</a></h2>
+
+</body>
+</html>
