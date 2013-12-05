caf4f820c3032e37325f39c746dc2999c75125dd
Dominik Niehus <dominik.niehus@coactum.de>
2013-12-05 16:31:58 +0100

    fixed defaults

--- platforms/default/main.php
+++ platforms/default/main.php
@@ -1,4 +1,5 @@
 <?php
+include_once dirname(__FILE__) . "/../../depending/vendor/open-steam/phpsteam/etc/phpsteam.const.php";
 include_once dirname(__FILE__) . '/etc/local.def.php';
 include_once dirname(__FILE__) . '/etc/default.def.php';
 
@@ -10,15 +11,15 @@ include_once PATH_ETC . 'php.conf.php';
 include_once PATH_ETC . 'core.conf.php';
 
 if (strstr(strtolower(urldecode($_SERVER['REQUEST_URI'])), "/asset/")) {
-		$cache_status = CacheSettings::caching_enabled();
-		CacheSettings::enable_caching();
-		$cache = get_cache_function("asset", 3600 );
-		$em = ExtensionMaster::getInstance();
-		$cache->call(array(&$em, "handleRequest"));
-		if (!$cache_status) {
-			CacheSettings::disable_caching();
-		}
+	$cache_status = CacheSettings::caching_enabled();
+	CacheSettings::enable_caching();
+	$cache = get_cache_function("asset", 3600 );
+	$em = ExtensionMaster::getInstance();
+	$cache->call(array(&$em, "handleRequest"));
+	if (!$cache_status) {
+		CacheSettings::disable_caching();
+    }
 } else {
-	ExtensionMaster::getInstance()->handleRequest();
+    ExtensionMaster::getInstance()->handleRequest();
 }
 ?>
