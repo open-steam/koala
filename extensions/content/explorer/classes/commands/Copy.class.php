217a9bed3c9e5a588e976a4ed0421135c78a80bb
Dominik Niehus <dominik.niehus@coactum.de>
2013-12-05 14:10:20 +0100

    adapt api

--- extensions/content/explorer/classes/commands/Copy.class.php
+++ extensions/content/explorer/classes/commands/Copy.class.php
@@ -1,37 +1,39 @@
 <?php
 namespace Explorer\Commands;
-class Copy extends \AbstractCommand implements \IAjaxCommand {
+class Copy extends \AbstractCommand implements \IAjaxCommand
+{
+    private $params;
+    private $id;
+    private $user;
+    private $success = true;
+    private $name;
 
-	private $params;
-	private $id;
-	private $user;
-        private $success = true;
-        private $name;
+    public function validateData(\IRequestObject $requestObject)
+    {
+        return true;
+    }
 
-	public function validateData(\IRequestObject $requestObject) {
-		return true;
-	}
-
-	public function processData(\IRequestObject $requestObject) {
-		$this->params = $requestObject->getParams();
-		$this->id = $this->params["id"];
-		$this->user = $GLOBALS["STEAM"]->get_current_steam_user();
-		$object = \steam_factory::get_object($GLOBALS["STEAM"]->get_id(), $this->id);
-		if(getObjectType($object) === "portal" ){
+    public function processData(\IRequestObject $requestObject)
+    {
+        $this->params = $requestObject->getParams();
+        $this->id = $this->params["id"];
+        $this->user = $GLOBALS["STEAM"]->get_current_steam_user();
+        $object = \steam_factory::get_object($GLOBALS["STEAM"]->get_id(), $this->id);
+        if (getObjectType($object) === "portal" ) {
                     $portalInstance = \PortletTopic::getInstance();
                     $portalObjectId = $object->get_id();
                     \ExtensionMaster::getInstance()->callCommand("PortalCopy", "Portal", array("id" => $portalObjectId));
-                } else if (getObjectType($object) === "pyramiddiscussion") {
+                } elseif (getObjectType($object) === "pyramiddiscussion") {
                     \ExtensionMaster::getInstance()->getExtensionById("Pyramiddiscussion")->copyPyramiddiscussion($object);
                 } else {
-                    if ($object instanceof \steam_link){
+                    if ($object instanceof \steam_link) {
                         $copy = \steam_factory::create_link($GLOBALS["STEAM"]->get_id(), $object->get_link_object());
                         $copy->set_name($object->get_name());
                         $copy->move($this->user);
-                    } else if ($object instanceof \steam_container) {
+                    } elseif ($object instanceof \steam_container) {
                         list($countObjects, $countSize) = $this->countInventoryRecursive($object);
                         if ($countObjects <= 500 && $countSize <= 524288000) {
-                            $copy = \steam_factory::create_copy($GLOBALS["STEAM"]->get_id(), $object);
+                            $copy = $object->copy();
                             $copy->move($this->user);
                         } else {
                             $this->success = false;
@@ -41,10 +43,11 @@ class Copy extends \AbstractCommand implements \IAjaxCommand {
                         $copy = $object->copy();
                         $copy->move($this->user);
                     }
-		}
-	}
+        }
+    }
 
-        private function countInventoryRecursive($object, $countObjects = 0, $countSize = 0) {
+        private function countInventoryRecursive($object, $countObjects = 0, $countSize = 0)
+        {
             $inventory = $object->get_inventory();
             $countObjects = $countObjects + count($inventory);
             foreach ($inventory as $element) {
@@ -53,25 +56,28 @@ class Copy extends \AbstractCommand implements \IAjaxCommand {
                 }
                 if ($element instanceof \steam_document) {
                     $countSize = $countSize + $element->get_content_size();
-                } else if ($element instanceof \steam_container) {
+                } elseif ($element instanceof \steam_container) {
                     list($countObjects, $countSize) = $this->countInventoryRecursive($element, $countObjects, $countSize);
                 }
             }
+
             return array($countObjects, $countSize);
         }
 
-	public function ajaxResponse(\AjaxResponseObject $ajaxResponseObject) {
+    public function ajaxResponse(\AjaxResponseObject $ajaxResponseObject)
+    {
             if ($this->success === true) {
-		$ajaxResponseObject->setStatus("ok");
-		$jswrapper = new \Widgets\JSWrapper();
-		$clipboardModel = new \Explorer\Model\Clipboard($this->user);
-		$js = "document.getElementById('clipboardIconbarWrapper').innerHTML = '" . $clipboardModel->getIconbarHtml() . "';" ;
+        $ajaxResponseObject->setStatus("ok");
+        $jswrapper = new \Widgets\JSWrapper();
+        $clipboardModel = new \Explorer\Model\Clipboard($this->user);
+        $js = "document.getElementById('clipboardIconbarWrapper').innerHTML = '" . $clipboardModel->getIconbarHtml() . "';" ;
                 $jswrapper->setJs($js);
-		$ajaxResponseObject->addWidget($jswrapper);
-		return $ajaxResponseObject;
+        $ajaxResponseObject->addWidget($jswrapper);
+
+        return $ajaxResponseObject;
             } else {
                 $ajaxResponseObject->setStatus("ok");
-		$jswrapper = new \Widgets\JSWrapper();
+        $jswrapper = new \Widgets\JSWrapper();
                 $js = "
                     if ($('#error').length == 0) {
                         $('#content').prepend('<p id=\"error\" style=\"\">Es ist nicht möglich Ordner zu kopieren, die mehr als 200 Objekte enthalten oder deren Dateigröße 500MB übersteigt.<br>Folgende Ordner wurden nicht kopiert: " . $this->name . "</p>');
@@ -85,9 +91,9 @@ class Copy extends \AbstractCommand implements \IAjaxCommand {
                     $('.popupmenuwapper').hide();
                     $('.popupmenuanker').removeClass('open')";
                 $jswrapper->setJs($js);
-		$ajaxResponseObject->addWidget($jswrapper);
-		return $ajaxResponseObject;
+        $ajaxResponseObject->addWidget($jswrapper);
+
+        return $ajaxResponseObject;
             }
-	}
+    }
 }
-?>
