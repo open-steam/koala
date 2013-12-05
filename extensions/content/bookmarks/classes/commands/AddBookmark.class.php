e2b676c17500cb9e57d3122722cb93c9d1ac3726
Dominik Niehus <dominik.niehus@coactum.de>
2013-12-05 14:08:45 +0100

    adapt api

--- extensions/content/bookmarks/classes/commands/AddBookmark.class.php
+++ extensions/content/bookmarks/classes/commands/AddBookmark.class.php
@@ -1,53 +1,50 @@
 <?php
 namespace Bookmarks\Commands;
-class AddBookmark extends \AbstractCommand implements \IAjaxCommand {
-	
-	private $params;
-	private $id;
-	
-	public function validateData(\IRequestObject $requestObject) {
-		return true;
-	}
-	
-	public function processData(\IRequestObject $requestObject) {		
-		$this->params = $requestObject->getParams();
-		
-		
-		
-		$this->id = $this->params["id"];
-		$bookmarks = $GLOBALS["STEAM"]->get_current_steam_user()->get_attribute(USER_BOOKMARKROOM);
-		
-		
-		
-		$object = \steam_factory::get_object($GLOBALS["STEAM"]->get_id(), $this->id);
-		
-		if($object instanceof \steam_link) {
-			$link = \steam_factory::create_link($GLOBALS["STEAM"]->get_id(), $object->get_link_object());
-		} else if ($object instanceof \steam_docextern) {
-			$link = \steam_factory::create_copy($GLOBALS["STEAM"]->get_id(), $object);
-		} else if ($object instanceof \steam_exit) {
-			$link = \steam_factory::create_link($GLOBALS["STEAM"]->get_id(), $object->get_exit());
-		} else {
-			$link = \steam_factory::create_link($GLOBALS["STEAM"]->get_id(), $object);
-		}
-		$link->set_attribute(OBJ_DESC,  $object->get_attribute(OBJ_DESC));
-		$link->set_attribute(DOC_MIME_TYPE,  $object->get_attribute(DOC_MIME_TYPE));
-		$link->move($bookmarks);
-	}
-	
-	public function ajaxResponse(\AjaxResponseObject $ajaxResponseObject) {
-		$ajaxResponseObject->setStatus("ok");
-		$rawHtml = new \Widgets\RawHtml();
-		$rawHtml->setHtml(\Bookmarks\Model\Bookmark::getMarkerHtml($this->id));
-		$ajaxResponseObject->addWidget($rawHtml);
-		$path= $this->params["path"];
-	/*	if(strpos($path,"bookmarks")!==false){
-			$jsWrapper = new \Widgets\JSWrapper();
-			$ajaxResponseObject->addWidget($jsWrapper);
-		}*/
-		
-		
-		return $ajaxResponseObject;
-	}
+class AddBookmark extends \AbstractCommand implements \IAjaxCommand
+{
+    private $params;
+    private $id;
+
+    public function validateData(\IRequestObject $requestObject)
+    {
+        return true;
+    }
+
+    public function processData(\IRequestObject $requestObject)
+    {
+        $this->params = $requestObject->getParams();
+
+        $this->id = $this->params["id"];
+        $bookmarks = $GLOBALS["STEAM"]->get_current_steam_user()->get_attribute(USER_BOOKMARKROOM);
+
+        $object = \steam_factory::get_object($GLOBALS["STEAM"]->get_id(), $this->id);
+
+        if ($object instanceof \steam_link) {
+            $link = \steam_factory::create_link($GLOBALS["STEAM"]->get_id(), $object->get_link_object());
+        } elseif ($object instanceof \steam_docextern) {
+            $link = $object->copy();
+        } elseif ($object instanceof \steam_exit) {
+            $link = \steam_factory::create_link($GLOBALS["STEAM"]->get_id(), $object->get_exit());
+        } else {
+            $link = \steam_factory::create_link($GLOBALS["STEAM"]->get_id(), $object);
+        }
+        $link->set_attribute(OBJ_DESC,  $object->get_attribute(OBJ_DESC));
+        $link->set_attribute(DOC_MIME_TYPE,  $object->get_attribute(DOC_MIME_TYPE));
+        $link->move($bookmarks);
+    }
+
+    public function ajaxResponse(\AjaxResponseObject $ajaxResponseObject)
+    {
+        $ajaxResponseObject->setStatus("ok");
+        $rawHtml = new \Widgets\RawHtml();
+        $rawHtml->setHtml(\Bookmarks\Model\Bookmark::getMarkerHtml($this->id));
+        $ajaxResponseObject->addWidget($rawHtml);
+        $path= $this->params["path"];
+    /*	if (strpos($path,"bookmarks")!==false) {
+            $jsWrapper = new \Widgets\JSWrapper();
+            $ajaxResponseObject->addWidget($jsWrapper);
+        }*/
+
+        return $ajaxResponseObject;
+    }
 }
-?>
