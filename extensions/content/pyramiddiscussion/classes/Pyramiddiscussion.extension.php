70b8b15b93f47689e3d8586b6841648bd0f7e094
Dominik Niehus <dominik.niehus@coactum.de>
2013-12-05 14:24:03 +0100

    adapt api

--- extensions/content/pyramiddiscussion/classes/Pyramiddiscussion.extension.php
+++ extensions/content/pyramiddiscussion/classes/Pyramiddiscussion.extension.php
@@ -1,82 +1,96 @@
 <?php
-class Pyramiddiscussion extends AbstractExtension implements IObjectExtension{
+class Pyramiddiscussion extends AbstractExtension implements IObjectExtension
+{
+    public function getName()
+    {
+        return "Pyramiddiscussion";
+    }
 
-	public function getName() {
-		return "Pyramiddiscussion";
-	}
+    public function getDesciption()
+    {
+        return "Extension for Pyramiddiscussions.";
+    }
 
-	public function getDesciption() {
-		return "Extension for Pyramiddiscussions.";
-	}
+    public function getVersion()
+    {
+        return "v1.0.0";
+        /*
+        possible extensions in future versions:
+         - pictures in positions
+         - anonymous pyramid
+        - editing state on position in pyramid view
+        - polling to test for pyramid state changes
+         */
+    }
 
-	public function getVersion() {
-		return "v1.0.0";
-		/* 
-		possible extensions in future versions:
- 		- pictures in positions
- 		- anonymous pyramid
-		- editing state on position in pyramid view
-		- polling to test for pyramid state changes
-		 */
-	}
+    public function getAuthors()
+    {
+        $result = array();
+        $result[] = new Person("Petertonkoker", "Jan", "janp@mail.uni-paderborn.de");
 
-	public function getAuthors() {
-		$result = array();
-		$result[] = new Person("Petertonkoker", "Jan", "janp@mail.uni-paderborn.de");
-		return $result;
-	}
-	
-	public function getObjectReadableName() {
-		return "Pyramidendiskussion";
-	}
-	
-	public function getObjectReadableDescription() {
-		return "Darstellung von Pyramidendiskussionen.";
-	}
-	
-	public function getObjectIconUrl() {
-		return $this->getAssetUrl() . "icons/pyramiddiscussion.png";
-	}
-	
-	public function getCreateNewCommand(IdRequestObject $idEnvironment) {
-		return new \Pyramiddiscussion\Commands\NewPyramiddiscussionForm();
-	}
-	
-	public function getCommandByObjectId(IdRequestObject $idRequestObject){
-		$pyramidObject = steam_factory::get_object( $GLOBALS["STEAM"]->get_id(), $idRequestObject->getId() );
-		$pyramidType = $pyramidObject->get_attribute("OBJ_TYPE");
-		if ($pyramidType != "0" && strStartsWith($pyramidType, "container_pyramiddiscussion")) {
-			return new \Pyramiddiscussion\Commands\Index();
-		}
-		return null;
-	}
-	
-	public function getPriority() {
-		return 8;
-	}
-        
-        public function copyPyramiddiscussion($object) {
+        return $result;
+    }
+
+    public function getObjectReadableName()
+    {
+        return "Pyramidendiskussion";
+    }
+
+    public function getObjectReadableDescription()
+    {
+        return "Darstellung von Pyramidendiskussionen.";
+    }
+
+    public function getObjectIconUrl()
+    {
+        return $this->getAssetUrl() . "icons/pyramiddiscussion.png";
+    }
+
+    public function getCreateNewCommand(IdRequestObject $idEnvironment)
+    {
+        return new \Pyramiddiscussion\Commands\NewPyramiddiscussionForm();
+    }
+
+    public function getCommandByObjectId(IdRequestObject $idRequestObject)
+    {
+        $pyramidObject = steam_factory::get_object( $GLOBALS["STEAM"]->get_id(), $idRequestObject->getId() );
+        $pyramidType = $pyramidObject->get_attribute("OBJ_TYPE");
+        if ($pyramidType != "0" && strStartsWith($pyramidType, "container_pyramiddiscussion")) {
+            return new \Pyramiddiscussion\Commands\Index();
+        }
+
+        return null;
+    }
+
+    public function getPriority()
+    {
+        return 8;
+    }
+
+        public function copyPyramiddiscussion($object)
+        {
             $group = $object->get_attribute("PYRAMIDDISCUSSION_PRIVGROUP");
             $user = $GLOBALS["STEAM"]->get_current_steam_user();
-            
+
             if ($group->check_access(SANCTION_WRITE, $user)) {
                 $instances = $group->get_attribute("PYRAMIDDISCUSSION_INSTANCES");
                 if (!is_array($instances)) {
                     $instances = array($object->get_id());
                 }
 
-                $copy = \steam_factory::create_copy($GLOBALS["STEAM"]->get_id(), $object);
+                $copy = $object->copy();
                 $instances[] = $copy->get_id();
                 $group->set_attribute("PYRAMIDDISCUSSION_INSTANCES", $instances);
 
                 $copy->move($user);
             }
         }
-        
-        public function deletePyramiddiscussion($object) {
+
+        public function deletePyramiddiscussion($object)
+        {
             $group = $object->get_attribute("PYRAMIDDISCUSSION_PRIVGROUP");
             $user = $GLOBALS["STEAM"]->get_current_steam_user();
-            
+
             if ($group->check_access(SANCTION_WRITE, $user)) {
                 $id = $object->get_id();
                 $instances = $group->get_attribute("PYRAMIDDISCUSSION_INSTANCES");
@@ -101,4 +115,3 @@ class Pyramiddiscussion extends AbstractExtension implements IObjectExtension{
             }
         }
 }
-?>
