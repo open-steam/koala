498953f68e7a8899ecacf610ce75b3dc6670886e
Dominik Niehus <dominik.niehus@coactum.de>
2013-12-05 16:46:53 +0100

    security fix

--- extensions/content/webarena/classes/commands/Index.class.php
+++ extensions/content/webarena/classes/commands/Index.class.php
@@ -1,64 +1,68 @@
 <?php
 namespace Webarena\Commands;
-class Index extends \AbstractCommand implements \IFrameCommand {
-	
-	private $params;
-	private $id;
-	
-	public function validateData(\IRequestObject $requestObject) {
-		return true;
-	}
-	
-	public function processData(\IRequestObject $requestObject) {
-		$this->params = $requestObject->getParams();
-		isset($this->params[0]) ? $this->id = $this->params[0]: "";
-	}
-	
-	public function frameResponse(\FrameResponseObject $frameResponseObject) {
+class Index extends \AbstractCommand implements \IFrameCommand
+{
+    private $params;
+    private $id;
 
-		$myExtension = \Webarena::getInstance();
+    public function validateData(\IRequestObject $requestObject)
+    {
+        return true;
+    }
 
-		$obj = \steam_factory::get_object($GLOBALS[ "STEAM" ]->get_id(), $this->id);
+    public function processData(\IRequestObject $requestObject)
+    {
+        $this->params = $requestObject->getParams();
+        isset($this->params[0]) ? $this->id = $this->params[0]: "";
+    }
 
-		if ($obj->get_attribute("isWebarena") == 1) {
+    public function frameResponse(\FrameResponseObject $frameResponseObject)
+    {
+        $myExtension = \Webarena::getInstance();
 
-			$host = WEBARENA_HOST;
+        $obj = \steam_factory::get_object($GLOBALS[ "STEAM" ]->get_id(), $this->id);
+
+        if ($obj->get_attribute("isWebarena") == 1) {
+
+            $host = WEBARENA_HOST;
             $port = WEBARENA_PORT;
+            $uid = uidv4();
 
             if ($host == "localhost") {
-				$host = $_SERVER['HTTP_HOST'];
+                $host = $_SERVER['HTTP_HOST'];
             }
 
             $fp = fsockopen($host, $port);
 
-        	if ($fp) {
-                
-				$data = http_build_query(Array(
-					"id" => session_id(),
-					"username" => $_SESSION[ "LMS_USER" ]->get_login(),
-					"password" => $_SESSION[ "LMS_USER" ]->get_password()
-				));
+            if ($fp) {
 
-				// send the request headers:
-				fputs($fp, "POST /pushSession HTTP/1.1\r\n");
-				fputs($fp, "Host: ".$host."\r\n");
+                $data = http_build_query(
+                    array(
+                        "id" => $uid,
+                        "username" => $_SESSION[ "LMS_USER" ]->get_login(),
+                        "password" => $_SESSION[ "LMS_USER" ]->get_password()
+                    )
+                );
 
-				fputs($fp, "Content-type: application/x-www-form-urlencoded\r\n");
-				fputs($fp, "Content-length: ". strlen($data) ."\r\n");
-				fputs($fp, "Connection: close\r\n\r\n");
-				fputs($fp, $data);
+                // send the request headers:
+                fputs($fp, "POST /pushSession HTTP/1.1\r\n");
+                fputs($fp, "Host: ".$host."\r\n");
 
-			} else { 
-				throw new Exception("unable to connect to webarena server");
-			}
+                fputs($fp, "Content-type: application/x-www-form-urlencoded\r\n");
+                fputs($fp, "Content-length: ". strlen($data) ."\r\n");
+                fputs($fp, "Connection: close\r\n\r\n");
+                fputs($fp, $data);
 
-			fclose($fp);
-			
-			header("Location: http://".$host.":".$port."/room/".$this->id."#externalSession/".$_SESSION[ "LMS_USER" ]->get_login()."/".session_id());
-		}
+            } else {
+                throw new Exception("unable to connect to webarena server");
+            }
 
-		return $frameResponseObject;
+            fclose($fp);
 
-	}
+            header("Location: http://".$host.":".$port."/room/".$this->id."#externalSession/".$_SESSION[ "LMS_USER" ]->get_login()."/".$uid);
+        }
+
+        return $frameResponseObject;
+
+    }
 }
-?>
