98274595d416322cb4d2130140b9a7796e238367
Dominik Niehus <dominik.niehus@coactum.de>
2013-12-12 10:52:16 +0100

    more updater

--- tools/update.php
+++ tools/update.php
@@ -1,11 +1,16 @@
 #!/usr/bin/php
 <?php
-$pathComposer = __DIR__ . "/../";
+
+$pathBase = __DIR__ . "/../";
+
+echo "** updating koala **" . PHP_EOL;
+passthru("cd $pathBase; git reset --hard; git pull");
+echo PHP_EOL;
 
 echo "** installing or updating composer **" . PHP_EOL;
 
-if (file_exists($pathComposer . "composer.phar")) {
-    passthru("cd $pathComposer; php composer.phar --quiet self-update");
+if (file_exists($pathBase . "composer.phar")) {
+    passthru("cd $pathBase; php composer.phar --quiet self-update");
 } else {
     $returnStatus = false;
     exec('curl 2> /dev/null', $asdf, $returnStatus);
@@ -14,4 +19,7 @@ if (file_exists($pathComposer . "composer.phar")) {
     }
     passthru("cd $pathComposer; curl -s https://getcomposer.org/installer | php");
 }
+
+passthru("cd $pathBase; php composer.phar update");
+
 echo "*************************************" . PHP_EOL . PHP_EOL;
