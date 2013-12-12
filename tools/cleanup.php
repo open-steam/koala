f41249d35ab562ca134f15cc5ed2c486a693f2c6
Dominik Niehus <dominik.niehus@coactum.de>
2013-12-12 11:18:42 +0100

    adding cleanup code

--- tools/cleanup.php
+++ tools/cleanup.php
@@ -1,2 +1,11 @@
 #!/usr/bin/php
 <?php
+
+$pathBase = __DIR__ . "/../";
+
+passthru("rm -Rfv $pathBase/platforms/bid/cache/*");
+passthru("rm -Rfv $pathBase/platforms/bid/temp/*");
+passthru("rm -Rfv $pathBase/platforms/elab/cache/*");
+passthru("rm -Rfv $pathBase/platforms/elab/temp/*");
+passthru("rm -Rfv $pathBase/platforms/schulen-gt/cache/*");
+passthru("rm -Rfv $pathBase/platforms/schulen-gt/temp/*");
