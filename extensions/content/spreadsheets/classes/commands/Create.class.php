0ccbfb7774ef819fa22a025dee17c84afb954e36
Dominik Niehus <dominik.niehus@coactum.de>
2014-01-17 22:17:54 +0100

    fixes

--- extensions/content/spreadsheets/classes/commands/Create.class.php
+++ extensions/content/spreadsheets/classes/commands/Create.class.php
@@ -5,72 +5,85 @@ namespace Spreadsheets\Commands;
  * This Command creates a new spreadsheet document with the given ID.
  * The Command will be called from the dialog in "NewSpreadsheetForm"
  */
-class Create extends \AbstractCommand implements \IAjaxCommand {
+class Create extends \AbstractCommand implements \IAjaxCommand
+{
+    private $params, $id;
 
-	private $params, $id;
-	
-	public function validateData(\IRequestObject $requestObject) {
-		return true;
-	}
-	
-	public function processData(\IRequestObject $requestObject) {
-		if ($requestObject instanceof \AjaxRequestObject) {
-			$this->params = $requestObject->getParams();
-			isset($this->params["id"]) ? $this->id = $this->params["id"]: "";
-		}
-	}
+    public function validateData(\IRequestObject $requestObject)
+    {
+        return true;
+    }
 
-	public function ajaxResponse(\AjaxResponseObject $ajaxResponseObject) {
-		$this->newSpreadsheet($this->params["title"]);
-		$ajaxResponseObject->setStatus("ok");
+    public function processData(\IRequestObject $requestObject)
+    {
+        if ($requestObject instanceof \AjaxRequestObject) {
+            $this->params = $requestObject->getParams();
+            isset($this->params["id"]) ? $this->id = $this->params["id"]: "";
+        }
+    }
 
-		//user will be forwarded to the explorer after creation of the document
-		$path = PATH_URL;
-		$jswrapper = new \Widgets\JSWrapper();
-		$jswrapper->setJs(<<<END
-		closeDialog();
-		location.href = '{$path}explorer/Index/{$this->id}';
+    public function ajaxResponse(\AjaxResponseObject $ajaxResponseObject)
+    {
+        $this->newSpreadsheet($this->params["title"]);
+        $ajaxResponseObject->setStatus("ok");
+
+        //user will be forwarded to the explorer after creation of the document
+        $path = PATH_URL;
+        $jswrapper = new \Widgets\JSWrapper();
+        $jswrapper->setJs(<<<END
+        closeDialog();
+        location.href = '{$path}explorer/Index/{$this->id}';
 END
-		);
-		$ajaxResponseObject->addWidget($jswrapper);
-		return $ajaxResponseObject;
-	}
+        );
+        $ajaxResponseObject->addWidget($jswrapper);
 
-	private function newSpreadsheet($name) {
-		//set default size for the spreadsheet if no size is specified
-		isset($this->params["rows"]) ? $row_count = $this->params["rows"]: 10;
-		isset($this->params["columns"]) ? $col_count = $this->params["columns"]: 5;
+        return $ajaxResponseObject;
+    }
 
-		//create new sTeam document in current container
-		$spreadsheetExtension = \Spreadsheets::getInstance();		
-		$container = \steam_factory::get_object( $GLOBALS["STEAM"]->get_id(), $this->id);
-		$document = \steam_factory::create_document($GLOBALS["STEAM"]->get_id(), $name, "", "application/json", $container);
-		$document->set_attribute("OBJ_TYPE", "document_spreadsheet");
-		$document->set_content($this->createJsonDocument(1, $row_count, $col_count));
-	}
+    private function newSpreadsheet($name)
+    {
+        //set default size for the spreadsheet if no size is specified
+        isset($this->params["rows"]) ? $row_count = $this->params["rows"]: 10;
+        isset($this->params["columns"]) ? $col_count = $this->params["columns"]: 5;
 
-	private function createJsonDocument($sheet_count, $row_count, $col_count) {
-		//creates a new spreadsheet document as a JSON-Object
+        if (empty($row_count)) {
+            $row_count = 10;
+        }
+
+        if (empty($col_count)) {
+            $col_count = 5;
+        }
+
+        //create new sTeam document in current container
+        $spreadsheetExtension = \Spreadsheets::getInstance();
+        $container = \steam_factory::get_object( $GLOBALS["STEAM"]->get_id(), $this->id);
+        $document = \steam_factory::create_document($GLOBALS["STEAM"]->get_id(), $name, "", "application/json", $container);
+        $document->set_attribute("OBJ_TYPE", "document_spreadsheet");
+        $document->set_content($this->createJsonDocument(1, $row_count, $col_count));
+    }
+
+    private function createJsonDocument($sheet_count, $row_count, $col_count)
+    {
+        //creates a new spreadsheet document as a JSON-Object
         $sheets = array();
         $cols = array();
-		$col_info = array();
-		$rows = array();
-		$users = array(''=>'');
+        $col_info = array();
+        $rows = array();
+        $users = array(''=>'');
 
-		for ($col = 0; $col < $col_count; $col++) {
-			$col_info[] = array('size' => 120);
-			$cols[] = array('value' => "", 'style' =>  array('flags' => array('styleLeft' => true)));
-		}
-        
-		for ($row = 0; $row < $row_count; $row++) {
-			$rows[] = array('cells' => $cols, 'size' => 20);
-		}
-        
+        for ($col = 0; $col < $col_count; $col++) {
+            $col_info[] = array('size' => 120);
+            $cols[] = array('value' => "", 'style' =>  array('flags' => array('styleLeft' => true)));
+        }
+
+        for ($row = 0; $row < $row_count; $row++) {
+            $rows[] = array('cells' => $cols, 'size' => 20);
+        }
+
         for ($sheet = 0; $sheet < $sheet_count; $sheet++) {
-	        $sheets[] = array('rows' => $rows, 'columns' => $col_info);
-		}
+            $sheets[] = array('rows' => $rows, 'columns' => $col_info);
+        }
 
-		return json_encode(array('sheets' => $sheets, 'users' => $users));
-	}
+        return json_encode(array('sheets' => $sheets, 'users' => $users));
+    }
 }
-?>
--- extensions/content/spreadsheets/classes/commands/Index.class.php
+++ extensions/content/spreadsheets/classes/commands/Index.class.php
@@ -5,196 +5,200 @@ namespace Spreadsheets\Commands;
  * This Command perpares the document with the given ID for realtime-editing
  * and shows the spreadsheet editor.
  */
-class Index extends \AbstractCommand implements \IFrameCommand {
-	
-	private $params;
-	private $document;
-	private $write_access;
-        private $NodeServer = SPREADSHEETS_RT_SERVER;
-	
-        
-	public function validateData(\IRequestObject $requestObject) {
-		return true;
-	}
-	
-	public function processData(\IRequestObject $requestObject) {
-		$this->params = $requestObject->getParams();
-		
-		if (isset($this->params[0])) {
-			$user = $GLOBALS["STEAM"]->get_current_steam_user();
-			$this->id = $this->params[0];
-			$this->document = \steam_factory::get_object($GLOBALS["STEAM"]->get_id(), $this->id);
-			$this->write_access = $this->document->check_access_write($user);
+class Index extends \AbstractCommand implements \IFrameCommand
+{
+    private $params;
+    private $document;
+    private $write_access;
+    private $NodeServer = SPREADSHEETS_RT_SERVER;
 
-			//see if the document already exists on the node.js server
-			$response = file_get_contents("http://$this->NodeServer/doc/exists/$this->id");
-			if ($response != "Document exists") {
-				//set the document if it doesn't exist
-				$this->setNodeDocument($this->document->get_content());
-			}
+    public function validateData(\IRequestObject $requestObject)
+    {
+        return true;
+    }
 
-			if ($this->write_access) {
-				$this->document->set_attribute("RT_EDIT", 1);
-			}
+    public function processData(\IRequestObject $requestObject)
+    {
+        $this->params = $requestObject->getParams();
 
-			//push session to node server
-			$host = SPREADSHEETS_HOST;
+        if (isset($this->params[0])) {
+            $user = $GLOBALS["STEAM"]->get_current_steam_user();
+            $this->id = $this->params[0];
+            $this->document = \steam_factory::get_object($GLOBALS["STEAM"]->get_id(), $this->id);
+            $this->write_access = $this->document->check_access_write($user);
+
+            //see if the document already exists on the node.js server
+            $response = file_get_contents("http://$this->NodeServer/doc/exists/$this->id");
+            if ($response != "Document exists") {
+                //set the document if it doesn't exist
+                $this->setNodeDocument($this->document->get_content());
+            }
+
+            if ($this->write_access) {
+                $this->document->set_attribute("RT_EDIT", 1);
+            }
+
+            //push session to node server
+            $host = SPREADSHEETS_HOST;
                         $port = SPREADSHEETS_PORT;
                         $fp = fsockopen($host, $port);
                         if ($fp) {
-				$data = http_build_query(Array(
-					"id" => session_id(),
-					"username" => $_SESSION[ "LMS_USER" ]->get_login(),
-					"password" => $_SESSION[ "LMS_USER" ]->get_password()
-				));
+                $data = http_build_query(Array(
+                    "id" => session_id(),
+                    "username" => $_SESSION[ "LMS_USER" ]->get_login(),
+                    "password" => $_SESSION[ "LMS_USER" ]->get_password()
+                ));
 
-				// send the request headers:
-				fputs($fp, "POST /pushSession HTTP/1.1\r\n");
-				fputs($fp, "Host: ".$host."\r\n");
+                // send the request headers:
+                fputs($fp, "POST /pushSession HTTP/1.1\r\n");
+                fputs($fp, "Host: ".$host."\r\n");
 
-				fputs($fp, "Content-type: application/x-www-form-urlencoded\r\n");
-				fputs($fp, "Content-length: ". strlen($data) ."\r\n");
-				fputs($fp, "Connection: close\r\n\r\n");
-				fputs($fp, $data);
-			} else {
+                fputs($fp, "Content-type: application/x-www-form-urlencoded\r\n");
+                fputs($fp, "Content-length: ". strlen($data) ."\r\n");
+                fputs($fp, "Connection: close\r\n\r\n");
+                fputs($fp, $data);
+            } else {
                                 throw new \Exception("unable to connect to spreadsheets server");
                         }
-		}		
-	}
-	
-	public function frameResponse(\FrameResponseObject $frameResponseObject) {
-                if(!defined("SPREADSHEETS_RT_SERVER")){
+        }
+    }
+
+    public function frameResponse(\FrameResponseObject $frameResponseObject)
+    {
+                if (!defined("SPREADSHEETS_RT_SERVER")) {
                     echo("Error<br>");
                     echo("default.def.php does not exist for spreadsheets extension<br>");
                     echo("copy default.def.php.example to default.def.php in spreadsheets dir<br>");
                     echo("and change server ip<br>");
                     die("");
                 }
-                
+
                 $doc_title = $this->document->get_name();
-		if (!$this->write_access) {
-			$doc_title .= " (schreibgeschützt)";
-		}
-		$rawWidget = $this->displaySpreadsheet();
-		$frameResponseObject->setTitle($doc_title);
-		$frameResponseObject->setHeadline($doc_title);
-		$frameResponseObject->addWidget($rawWidget);
-		return $frameResponseObject;
-	}
-	
-	private function displaySpreadsheet() {
-		$spreadsheetExtension = \Spreadsheets::getInstance();
+        if (!$this->write_access) {
+            $doc_title .= " (schreibgeschützt)";
+        }
+        $rawWidget = $this->displaySpreadsheet();
+        $frameResponseObject->setTitle($doc_title);
+        $frameResponseObject->setHeadline($doc_title);
+        $frameResponseObject->addWidget($rawWidget);
 
-		// addJS did not work for some reason with theese files, so i put them 
-		// into the asset directory as a workaround
-		/*$spreadsheetExtension->addJS("jquery.sheet.js");
-		$spreadsheetExtension->addJS("parser.js");
-		$spreadsheetExtension->addJS("raphael-min.js");
-		$spreadsheetExtension->addJS("g.raphael-min.js");*/
+        return $frameResponseObject;
+    }
 
-		$spreadsheetExtension->addJS();
-		$spreadsheetExtension->addCSS();
-		/*$spreadsheetExtension->addCSS("jquery-ui.css");
-		$spreadsheetExtension->addCSS("jquery.sheet.css");
-		$spreadsheetExtension->addCSS("jquery.colorPicker.css");*/
-		
-		$content = $spreadsheetExtension->loadTemplate("spreadsheets_index.template.html");
-		$user_name = $GLOBALS["STEAM"]->get_login_user_name();
-		$user = $GLOBALS["STEAM"]->get_current_steam_user();
-		$extension_url = $spreadsheetExtension->getExtensionUrl();
-	
-		//js files for jQuery.sheet
-		$content->setCurrentBlock("BLOCK_SPREADSHEET_SCRIPTS");
-		$content->setVariable("SCRIPT_SRC", $extension_url . "asset/js/jQuery.sheet/jquery.sheet.js");
-		$content->parse("BLOCK_SPREADSHEET_SCRIPTS");
-		
-		$content->setCurrentBlock("BLOCK_SPREADSHEET_SCRIPTS");
-		$content->setVariable("SCRIPT_SRC", $extension_url . "asset/js/jQuery.sheet/parser.js");
-		$content->parse("BLOCK_SPREADSHEET_SCRIPTS");
+    private function displaySpreadsheet()
+    {
+        $spreadsheetExtension = \Spreadsheets::getInstance();
 
-		$content->setCurrentBlock("BLOCK_SPREADSHEET_SCRIPTS");
-		$content->setVariable("SCRIPT_SRC", $extension_url . "asset/js/jQuery.sheet/raphael-min.js");
-		$content->parse("BLOCK_SPREADSHEET_SCRIPTS");
-		
-		$content->setCurrentBlock("BLOCK_SPREADSHEET_SCRIPTS");
-		$content->setVariable("SCRIPT_SRC", $extension_url . "asset/js/jQuery.sheet/g.raphael-min.js");
-		$content->parse("BLOCK_SPREADSHEET_SCRIPTS");
+        // addJS did not work for some reason with theese files, so i put them
+        // into the asset directory as a workaround
+        /*$spreadsheetExtension->addJS("jquery.sheet.js");
+        $spreadsheetExtension->addJS("parser.js");
+        $spreadsheetExtension->addJS("raphael-min.js");
+        $spreadsheetExtension->addJS("g.raphael-min.js");*/
 
-		$content->setCurrentBlock("BLOCK_SPREADSHEET_SCRIPTS");
-		$content->setVariable("SCRIPT_SRC", $extension_url . "asset/js/jQuery.sheet/jquery.colorPicker.min.js");
-		$content->parse("BLOCK_SPREADSHEET_SCRIPTS");
+        $spreadsheetExtension->addJS();
+        $spreadsheetExtension->addCSS();
+        /*$spreadsheetExtension->addCSS("jquery-ui.css");
+        $spreadsheetExtension->addCSS("jquery.sheet.css");
+        $spreadsheetExtension->addCSS("jquery.colorPicker.css");*/
 
-		//german localization
-		$content->setCurrentBlock("BLOCK_SPREADSHEET_SCRIPTS");
-		$content->setVariable("SCRIPT_SRC", $extension_url . "asset/js/jQuery.sheet/lang_de.js");
-		$content->parse("BLOCK_SPREADSHEET_SCRIPTS");
+        $content = $spreadsheetExtension->loadTemplate("spreadsheets_index.template.html");
+        $user_name = $GLOBALS["STEAM"]->get_login_user_name();
+        $user = $GLOBALS["STEAM"]->get_current_steam_user();
+        $extension_url = $spreadsheetExtension->getExtensionUrl();
 
-		//js files for ShareJS
-		$content->setCurrentBlock("BLOCK_SPREADSHEET_SCRIPTS");
-		$content->setVariable("SCRIPT_SRC", "http://$this->NodeServer/channel/bcsocket.js");
-		$content->parse("BLOCK_SPREADSHEET_SCRIPTS");
+        //js files for jQuery.sheet
+        $content->setCurrentBlock("BLOCK_SPREADSHEET_SCRIPTS");
+        $content->setVariable("SCRIPT_SRC", $extension_url . "asset/js/jQuery.sheet/jquery.sheet.js");
+        $content->parse("BLOCK_SPREADSHEET_SCRIPTS");
 
-		$content->setCurrentBlock("BLOCK_SPREADSHEET_SCRIPTS");
-		$content->setVariable("SCRIPT_SRC", "http://$this->NodeServer/share/share.uncompressed.js");
-		$content->parse("BLOCK_SPREADSHEET_SCRIPTS");
-		
-		$content->setCurrentBlock("BLOCK_SPREADSHEET_SCRIPTS");
-		$content->setVariable("SCRIPT_SRC", "http://$this->NodeServer/share/json.uncompressed.js");
-		$content->parse("BLOCK_SPREADSHEET_SCRIPTS");
+        $content->setCurrentBlock("BLOCK_SPREADSHEET_SCRIPTS");
+        $content->setVariable("SCRIPT_SRC", $extension_url . "asset/js/jQuery.sheet/parser.js");
+        $content->parse("BLOCK_SPREADSHEET_SCRIPTS");
 
-		//CSS Files for jQuery.sheet
-		$content->setCurrentBlock("BLOCK_SPREADSHEET_CSS");
-		$content->setVariable("CSS_HREF", $extension_url . "asset/js/jQuery.sheet/jquery.sheet.css");
-		$content->parse("BLOCK_SPREADSHEET_CSS");
+        $content->setCurrentBlock("BLOCK_SPREADSHEET_SCRIPTS");
+        $content->setVariable("SCRIPT_SRC", $extension_url . "asset/js/jQuery.sheet/raphael-min.js");
+        $content->parse("BLOCK_SPREADSHEET_SCRIPTS");
 
-		$content->setCurrentBlock("BLOCK_SPREADSHEET_CSS");
-		$content->setVariable("CSS_HREF", $extension_url . "asset/js/jQuery.sheet/jquery-ui.css");
-		$content->parse("BLOCK_SPREADSHEET_CSS");
+        $content->setCurrentBlock("BLOCK_SPREADSHEET_SCRIPTS");
+        $content->setVariable("SCRIPT_SRC", $extension_url . "asset/js/jQuery.sheet/g.raphael-min.js");
+        $content->parse("BLOCK_SPREADSHEET_SCRIPTS");
 
-		$content->setCurrentBlock("BLOCK_SPREADSHEET_CSS");
-		$content->setVariable("CSS_HREF", $extension_url . "asset/js/jQuery.sheet/jquery.colorPicker.css");
-		$content->parse("BLOCK_SPREADSHEET_CSS");
-		
-		//write document ID, document name and username in the script
-		$content->setCurrentBlock("BLOCK_SHEET_NAME_SCRIPT");
-		$content->setVariable("SHEET_ID", $this->document->get_id());
-		$content->setVariable("SHEET_TITLE", $this->document->get_name());
-		$content->setVariable("USER_NAME", $user_name);
-		$content->setVariable("RT_SERVER", $this->NodeServer);
-		if ($this->write_access) {
-			$content->setVariable("SHEET_EDITABLE", "true");
-		}
-		else {
-			$content->setVariable("SHEET_EDITABLE", "false");
-		}
-                
+        $content->setCurrentBlock("BLOCK_SPREADSHEET_SCRIPTS");
+        $content->setVariable("SCRIPT_SRC", $extension_url . "asset/js/jQuery.sheet/jquery.colorPicker.min.js");
+        $content->parse("BLOCK_SPREADSHEET_SCRIPTS");
+
+        //german localization
+        $content->setCurrentBlock("BLOCK_SPREADSHEET_SCRIPTS");
+        $content->setVariable("SCRIPT_SRC", $extension_url . "asset/js/jQuery.sheet/lang_de.js");
+        $content->parse("BLOCK_SPREADSHEET_SCRIPTS");
+
+        //js files for ShareJS
+        $content->setCurrentBlock("BLOCK_SPREADSHEET_SCRIPTS");
+        $content->setVariable("SCRIPT_SRC", "http://$this->NodeServer/channel/bcsocket.js");
+        $content->parse("BLOCK_SPREADSHEET_SCRIPTS");
+
+        $content->setCurrentBlock("BLOCK_SPREADSHEET_SCRIPTS");
+        $content->setVariable("SCRIPT_SRC", "http://$this->NodeServer/share/share.uncompressed.js");
+        $content->parse("BLOCK_SPREADSHEET_SCRIPTS");
+
+        $content->setCurrentBlock("BLOCK_SPREADSHEET_SCRIPTS");
+        $content->setVariable("SCRIPT_SRC", "http://$this->NodeServer/share/json.uncompressed.js");
+        $content->parse("BLOCK_SPREADSHEET_SCRIPTS");
+
+        //CSS Files for jQuery.sheet
+        $content->setCurrentBlock("BLOCK_SPREADSHEET_CSS");
+        $content->setVariable("CSS_HREF", $extension_url . "asset/js/jQuery.sheet/jquery.sheet.css");
+        $content->parse("BLOCK_SPREADSHEET_CSS");
+
+        $content->setCurrentBlock("BLOCK_SPREADSHEET_CSS");
+        $content->setVariable("CSS_HREF", $extension_url . "asset/js/jQuery.sheet/jquery-ui.css");
+        $content->parse("BLOCK_SPREADSHEET_CSS");
+
+        $content->setCurrentBlock("BLOCK_SPREADSHEET_CSS");
+        $content->setVariable("CSS_HREF", $extension_url . "asset/js/jQuery.sheet/jquery.colorPicker.css");
+        $content->parse("BLOCK_SPREADSHEET_CSS");
+
+        //write document ID, document name and username in the script
+        $content->setCurrentBlock("BLOCK_SHEET_NAME_SCRIPT");
+        $content->setVariable("SHEET_ID", $this->document->get_id());
+        $content->setVariable("SHEET_TITLE", $this->document->get_name());
+        $content->setVariable("USER_NAME", $user_name);
+        $content->setVariable("RT_SERVER", $this->NodeServer);
+        if ($this->write_access) {
+            $content->setVariable("SHEET_EDITABLE", "true");
+        } else {
+            $content->setVariable("SHEET_EDITABLE", "false");
+        }
+
         $content->setVariable("SESSION_COOKIE_NAME", SESSION_NAME);
 
-		$content->parse("BLOCK_SHEET_NAME_SCRIPT");
+        $content->parse("BLOCK_SHEET_NAME_SCRIPT");
 
-		//insert URLs for toolbar buttons
-		$content->setCurrentBlock("BLOCK_SHEET_TOOLBAR");
-		$content->setVariable("EXTENSION_URL", $extension_url);
-		$content->setVariable("SHEET_ID", $this->document->get_id());
-		$content->parse("BLOCK_SHEET_TOOLBAR");
-		
-		$rawWidget = new \Widgets\RawHtml();
-		$rawWidget->setHtml($content->get());
-		return $rawWidget;
-	}
+        //insert URLs for toolbar buttons
+        $content->setCurrentBlock("BLOCK_SHEET_TOOLBAR");
+        $content->setVariable("EXTENSION_URL", $extension_url);
+        $content->setVariable("SHEET_ID", $this->document->get_id());
+        $content->parse("BLOCK_SHEET_TOOLBAR");
 
-	// Sets the content for the document on the node.js server
-	private function setNodeDocument($content) {
-		//POST request to set the content
-		$opts = array('http' =>
-		    array(
-		        'method'  => 'POST',
-		        'header'  => 'Content-type: application/json',
-		        'content' => $content
-		    )
-		);
-		$context = stream_context_create($opts);
-		$response = file_get_contents("http://$this->NodeServer/doc/set/$this->id", false, $context);
-	}
+        $rawWidget = new \Widgets\RawHtml();
+        $rawWidget->setHtml($content->get());
+
+        return $rawWidget;
+    }
+
+    // Sets the content for the document on the node.js server
+    private function setNodeDocument($content)
+    {
+        //POST request to set the content
+        $opts = array('http' =>
+            array(
+                'method'  => 'POST',
+                'header'  => 'Content-type: application/json',
+                'content' => $content
+            )
+        );
+        $context = stream_context_create($opts);
+        $response = file_get_contents("http://$this->NodeServer/doc/set/$this->id", false, $context);
+    }
 }
-?>
