193dd89ac46ae53f75dff8b7203a26a81e2a8850
Dominik Niehus <dominik.niehus@coactum.de>
2014-01-21 13:35:28 +0100

    fixed error /var/www/koala/extensions/content/portletMsg/classes/PortletMsg.extension.php (41)

--- extensions/system/bid2PathCompatibility/classes/commands/Index.class.php
+++ extensions/system/bid2PathCompatibility/classes/commands/Index.class.php
@@ -4,92 +4,92 @@ class Index extends \AbstractCommand implements \IFrameCommand{
 	public function validateData(\IRequestObject $requestObject) {
 		return true;
 	}
-	
+
 	public function processData(\IRequestObject $requestObject) {
-        
+
         }
-	
+
 	public function frameResponse(\FrameResponseObject $frameResponseObject) {
                 //logging::write_log( LOG_ERROR, "b2pc-frameResponse"); //test
-                
+
                 $requestUrl = urldecode($_SERVER['REQUEST_URI']);
-                
+
                 //not tested
                 if(FALSE!==strpos($requestUrl, "index.php?object=/")){
                         $searchString = "index.php?object?=/";
                         $begin = strpos($requestUrl, $searchString) + strlen($searchString);
                         $this->redirectToSteamPath("/");
                 }
-                
+
                 //tested
                 if(FALSE!==strpos($requestUrl, "index.php?object=")){
                         $searchString = "index.php?object=";
                         $begin = strpos($requestUrl, $searchString) + strlen($searchString);
                         $this->redirectToObjectId(substr($requestUrl,$begin));
                 }
-                
+
                 //not tested
                 if(FALSE!==strpos($requestUrl, "/home/")){
                         $steamPath = $requestUrl;
                         $this->redirectToSteamPath($steamPath);
                 }
-            
+
                 //not tested
                 if(FALSE!==strpos($requestUrl, "/hilfe/")){
                         $steamPath = $requestUrl;
                         $this->redirectToSteamPath($steamPath);
                 }
-                
+
                 //not tested
                 if(FALSE!==strpos($requestUrl, "/schulen/")){
                         $steamPath = $requestUrl;
                         $this->redirectToSteamPath($steamPath);
                 }
-                
+
                 //not tested
                 if(FALSE!==strpos($requestUrl, "/lernstatt_intern/")){
                         $steamPath = $requestUrl;
                         $this->redirectToSteamPath($steamPath);
                 }
-                
+
                 //not tested
                 if(FALSE!==strpos($requestUrl, "/externe_partner/")){
                         $steamPath = $requestUrl;
                         $this->redirectToSteamPath($steamPath);
                 }
-                
+
                 //not tested
                 if(FALSE!==strpos($requestUrl, "/schulen/")){
                         $steamPath = $requestUrl;
                         $this->redirectToSteamPath($steamPath);
                 }
-                
+
                 //not tested
                 if(FALSE!==strpos($requestUrl, "/dialog/")){
                         $steamPath = $requestUrl;
                         $this->redirectToSteamPath($steamPath);
                 }
-            
+
                 //not tested
                 if(FALSE!==strpos($requestUrl, "/partner/")){
                         $steamPath = $requestUrl;
                         $this->redirectToSteamPath($steamPath);
                 }
-                
+
                 //not tested
                 if(FALSE!==strpos($requestUrl, "/projekte/")){
                         $steamPath = $requestUrl;
                         $this->redirectToSteamPath($steamPath);
                 }
-                
+
                 //not tested, download case
                 if(FALSE!==strpos($requestUrl, "/tools/get.php?object=")){
                         $searchString = "/tools/get.php?object=";
                         $begin = strpos($requestUrl, $searchString) + strlen($searchString);
-                        
+
                         $destination = substr($requestUrl,$begin);
                         $isObjectId = $destination === (string)(intval($destination)) ;
-                        
+
                         if($isObjectId){
                             //destination is a number
                             $this->redirectToDownloadObjectId($destination);
@@ -98,32 +98,32 @@ class Index extends \AbstractCommand implements \IFrameCommand{
                             $this->redirectToDownloadPath($destination);
                         }
                 }
-                
+
                 //TODO /download/
                 //not tested, download case
                 if(FALSE!==strpos($requestUrl, "/download/")){
                         $searchString = "/download/";
                         $begin = strpos($requestUrl, $searchString) + strlen($searchString);
-                        
+
                         $destination = substr($requestUrl,$begin);
-                        
+
                         $destArray = explode("/", $destination);
-                        
+
                         $objectId = $destArray[0];
                         $name = $destArray[1];
-                             
+
                         $this->redirectToDownloadObjectId($objectId, $name);
                 }
-                
+
                 die("URL-Umleitung fehlgeschlagen");
-            
+
                 $rawWidget = new \Widgets\RawHtml();
                 $rawWidget->setHtml("Test bid2PathCompatibility ".$requestUrl);
                 $frameResponseObject->addWidget($rawWidget);
                 return $frameResponseObject;
 	}
-        
-        
+
+
         /*
          * redirects to the extension/object with a steam path
          */
@@ -134,43 +134,57 @@ class Index extends \AbstractCommand implements \IFrameCommand{
                 header("Location: ".$url);
                 die;
         }
-        
-        
+
+
         /*
          * redirects to the extension/object with a object id
          */
         private function redirectToObjectId($objectId){
                 //logging::write_log( LOG_ERROR, "b2pc-redirectToObjectId"); //test
                 $objectId = (string)intval($objectId);
-                $url = \ExtensionMaster::getInstance()->getUrlForObjectId($objectId, "view");
-		header("Location: ".$url);
-                die;
+                try {
+                    $object = \steam_factory::get_object($GLOBALS["STEAM"]->get_id(), $objectId);
+                } catch (\Exception $e){
+                    $url = "/404/";
+                    header("Location: ".$url);
+                    die;
+                }
+                if ($object instanceof steam_object) {
+                    $url = \ExtensionMaster::getInstance()->getUrlForObjectId($objectId, "view");
+                    header("Location: ".$url);
+                    die;
+                } else {
+                    $url = "/404/";
+                    header("Location: ".$url);
+                    die;
+                }
+
         }
-        
+
         private function redirectToDownloadObjectId($objectId, $name=""){
                 //logging::write_log( LOG_ERROR, "b2pc-redirectToDownloadObjectId"); //test
                 $objectId = (string)intval($objectId);
-                
+
                 if($name==""){
                     $url = "/download/document/".$objectId;
                 }else{
                     $url = "/download/document/".$objectId."/".$name;
                 }
-                    
+
                 header("Location: ".$url);
                 die;
         }
-        
+
         private function redirectToDownloadPath($steamPath){
                 //logging::write_log( LOG_ERROR, "b2pc-redirectToDownloadPath"); //test
                 $url = "/download/document/".$this->getObjectId($steamPath);
                 header("Location: ".$url);
                 die;
         }
-        
-        
-        
-        
+
+
+
+
         /*
          * gets a objectId to a steam path
          */
@@ -180,11 +194,11 @@ class Index extends \AbstractCommand implements \IFrameCommand{
                 if (is_null($object)){
                     throw new \steam_exception;
                 }
-                
+
                 if (!is_object($object)){
                     throw new \steam_exception;
                 }
-                
+
                 return $object->get_id();
             }  catch (\Exception $e){
                 $url = "/404/";
@@ -192,8 +206,8 @@ class Index extends \AbstractCommand implements \IFrameCommand{
                 die;
             }
         }
-        
-        
+
+
         public function isGuestAllowed(\IRequestObject $requestObject) {
 		return true;
 	}
