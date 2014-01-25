f6f1165c9897dec7a7db6b08b4b83d110c8a6987
Dominik Niehus <dominik.niehus@coactum.de>
2014-01-26 00:40:46 +0100

    adding html5 video support for iPad, iPhone and Android

--- extensions/base/widgets/classes/Videoplayer.class.php
+++ extensions/base/widgets/classes/Videoplayer.class.php
@@ -29,14 +29,18 @@ class Videoplayer extends Widget {
     }
 
     public function getHtml() {
-
+        if (isset($_SERVER['HTTP_USER_AGENT']) && (strstr($_SERVER['HTTP_USER_AGENT'], "iPad") || strstr($_SERVER['HTTP_USER_AGENT'], "iPhone") || strstr($_SERVER['HTTP_USER_AGENT'], "Android"))) {
+            $this->getContent()->setCurrentBlock("BLOCK_VIDEO_HTML5");
+        } else {
+            $this->getContent()->setCurrentBlock("BLOCK_VIDEO");
+        }
 
         \lms_portal::get_instance()->add_javascript_src("flowplayer", PATH_URL . "styles/standard/javascript/Flowplayer/flowplayer-3.2.9.min.js");
         //if($this->target == ""){
         //    throw new \Exception("videoplayer: target-url isn't set.");
         //}else{
         $this->getContent()->setVariable("TARGET_URL", $this->target);
-        //}                
+        //}
         if ($this->swfPath == "") {
             $this->getContent()->setVariable("PATH_SWF", PATH_URL . "styles/standard/javascript/Flowplayer/flowplayer-3.2.10.swf");
         } else {
@@ -47,6 +51,13 @@ class Videoplayer extends Widget {
         $this->getContent()->setVariable("PLAYER_HEIGHT", $this->playerHeight);
 
         $this->getContent()->setVariable("PLAYERTAG_ID", uniqid());
+
+        if (isset($_SERVER['HTTP_USER_AGENT']) && (strstr($_SERVER['HTTP_USER_AGENT'], "iPad") || strstr($_SERVER['HTTP_USER_AGENT'], "iPhone") || strstr($_SERVER['HTTP_USER_AGENT'], "Android"))) {
+            $this->getContent()->parse("BLOCK_VIDEO_HTML5");
+        } else {
+            $this->getContent()->parse("BLOCK_VIDEO");
+        }
+
         return $this->getContent()->get();
     }
 
