f54e5ed1392cb9571f6120e43785761e27dadf07
Dominik Niehus <dominik.niehus@coactum.de>
2014-02-06 23:21:14 +0100

    fallback for old questionary objects

--- extensions/content/questionary/classes/Questionary.extension.php
+++ extensions/content/questionary/classes/Questionary.extension.php
@@ -1,42 +1,51 @@
 <?php
 class Questionary extends AbstractExtension implements IObjectExtension {
-	
+
 	public function getName() {
 		return "Questionary";
 	}
-	
+
 	public function getDesciption() {
 		return "Extension for questionary view.";
 	}
-	
+
 	public function getVersion() {
 		return "v1.0.0";
 	}
-	
+
 	public function getAuthors() {
 		$result = array();
-		$result[] = new Person("Niroshan", "Thillainathan", "n.thillainathan@campus.uni-paderborn.de");
+		$result[] = new Person("Dominik", "Niehus", "dominik.niehus@coactum.de");
 		return $result;
 	}
-	
+
 	public function getObjectReadableName() {
-		return null;
+		return "Fragebogen (alt)";
 	}
-	
+
 	public function getObjectReadableDescription() {
-		return null;
+		return "Fragebogen (alt)";
 	}
-	
+
 	public function getObjectIconUrl() {
 		return null;
 	}
-	
+
 	public function getCreateNewCommand(IdRequestObject $idEnvironment) {
 		return null;
 	}
-	
+
 	public function getCommandByObjectId(IdRequestObject $idRequestObject){
-		
+		$object = steam_factory::get_object( $GLOBALS["STEAM"]->get_id(), $idRequestObject->getId() );
+        $type = $object->get_attribute("bid:doctype");
+        if ($type === "questionary") {
+            return new \Questionary\Commands\Index();
+        }
+        return null;
 	}
+
+    public function getPriority() {
+        return 7.5;
+    }
 }
 ?>
