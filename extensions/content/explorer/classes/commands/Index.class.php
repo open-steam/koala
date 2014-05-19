3ab98c1a94b035872da56c4e05fcc8568ecbd2e2
Marcel Jakoblew <mjako@upb.de>
2014-05-19 13:44:35 +0200

    fix explorer view without rights

--- extensions/content/explorer/classes/commands/Index.class.php
+++ extensions/content/explorer/classes/commands/Index.class.php
@@ -216,7 +216,8 @@ class Index extends \AbstractCommand implements \IFrameCommand {
             $actionBar->setActions(array(array("name" => "Neu", "ajax" => array("onclick" => array("command" => "newElement", "params" => array("id" => $this->id), "requestType" => "popup"))),
                 array("name" => "Eigenschaften", "ajax" => array("onclick" => array("command" => "properties", "params" => array("id" => $this->id), "requestType" => "popup")))));
         } else {
-            $actionBar->setActions(array());
+            $actionBar->setActions(array(
+                array("name" => "Eigenschaften", "ajax" => array("onclick" => array("command" => "properties", "params" => array("id" => $this->id), "requestType" => "popup")))));
         }
 
         //$actionBar->setActions(array(array("name"=>"Neu", "ajax"=>array("onclick"=>array("command"=>"newelement"))), array("name"=>"Eigenschaften", "link"=>PATH_URL."explorer/properties/"), array("name"=>"Rechte", "link"=>PATH_URL."explorer/rights/")));
