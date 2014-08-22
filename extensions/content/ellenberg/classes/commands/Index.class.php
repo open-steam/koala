98b366ae4455c223a2ccc55f22ece8d59aa67c54
Andreas Schultz <sourcetree@haferfeld.de>
2014-08-22 12:57:05 +0200

    Ellenberg-tool extension updated

--- extensions/content/ellenberg/classes/commands/Index.class.php
+++ extensions/content/ellenberg/classes/commands/Index.class.php
@@ -17,9 +17,7 @@ class Index extends \AbstractCommand implements \IFrameCommand {
     public function processData(\IRequestObject $requestObject) {
         $this->params = $requestObject->getParams();
         isset($this->params[0]) ? $this->id = $this->params[0] : "";
-        if(!is_int($this->id) || $this->id == "" || $this->id == "0"){
-            throw new \Exception("There is no id given");
-        }
+        
     }
 
     public function frameResponse(\FrameResponseObject $frameResponseObject) {
