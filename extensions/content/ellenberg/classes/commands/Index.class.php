6a877adc6e7c964d54312d60d9e17166b62d4035
Andreas Schultz <sourcetree@haferfeld.de>
2014-08-24 22:09:54 +0200

    Ellenberg-tool extension updated - link conformed

--- extensions/content/ellenberg/classes/commands/Index.class.php
+++ extensions/content/ellenberg/classes/commands/Index.class.php
@@ -6,6 +6,7 @@ class Index extends \AbstractCommand implements \IFrameCommand {
 
     private $params;
     private $id;
+    
     //the basepath to all links with the ellenberg-tool
     private $ellenbergUrl = 'http://amole.cs.upb.de/webapp/#';
 
@@ -17,20 +18,21 @@ class Index extends \AbstractCommand implements \IFrameCommand {
     public function processData(\IRequestObject $requestObject) {
         $this->params = $requestObject->getParams();
         isset($this->params[0]) ? $this->id = $this->params[0] : "";
-        
     }
 
     public function frameResponse(\FrameResponseObject $frameResponseObject) {
 
         $object = \steam_factory::get_object($GLOBALS["STEAM"]->get_id(), $this->id);
+        //get the loginname of the current user to forward it to the ellenberg tool
+        $userLoginName = $_SESSION[ "LMS_USER" ]->get_login();
+        
         //generate the output
         //this is the header
         $headlineHtml = new \Widgets\Breadcrumb();
         $headlineHtml->setData(array("", array("name" => "<img src=\"" . PATH_URL . "explorer/asset/icons/mimetype/old/annotation.gif\" /> " . $object->get_name() . " - ein Ellenbergtool ")));
-
-        //headline
         $frameResponseObject->addWidget($headlineHtml);
 
+        //set some css code
         $cssStyles = new \Widgets\RawHtml();
         $cssStyles->setCss('
             .attribute{width:150px;float:left;padding-left:50px;padding-top:5px;} 
@@ -43,17 +45,17 @@ class Index extends \AbstractCommand implements \IFrameCommand {
         ');
         $frameResponseObject->addWidget($cssStyles);
 
-        $hintRemote = new \Widgets\RawHtml();
-        $hintRemote->setHtml('<p class ="breadcrumb">Mit den folgenden Links werden Sie zu einer externen Plattform weitergeleitet.</p>');
-        $frameResponseObject->addWidget($hintRemote);
+        $hintRemotePlatform = new \Widgets\RawHtml();
+        $hintRemotePlatform->setHtml('<p class ="breadcrumb">Mit den folgenden Links werden Sie zu einer externen Plattform weitergeleitet.</p>');
+        $frameResponseObject->addWidget($hintRemotePlatform);
         
         $generatorPlatform = new \Widgets\RawHtml();
-        $generatorPlatform->setHtml('<div class="attribute">Generator Plattform:</div><div class="value"><a target="_blank" href="'.$this->ellenbergUrl.'scenario/'.$object->get_Attribute("ELLENBERG_ID").'" > '.$this->ellenbergUrl.'scenario/'.$object->get_Attribute("ELLENBERG_ID").'</a></div>');
+        $generatorPlatform->setHtml('<div class="attribute">Generator Plattform:</div><div class="value"><a target="_blank" href="'.$this->ellenbergUrl.'scenario/'.$object->get_Attribute("ELLENBERG_ID").'/'.$userLoginName.'" > '.$this->ellenbergUrl.'scenario/'.$object->get_Attribute("ELLENBERG_ID").'/'.$userLoginName.'</a></div>');
         $frameResponseObject->addWidget($generatorPlatform);
         
-        $auswertungsPlatform = new \Widgets\RawHtml();
-        $auswertungsPlatform->setHtml('<div class="attribute">Auswertungs Plattform:</div><div class="value"><a target="_blank" href="'.$this->ellenbergUrl.'summary/'.$object->get_Attribute("ELLENBERG_ID").'" > '.$this->ellenbergUrl.'summary/'.$object->get_Attribute("ELLENBERG_ID").'</a></div>');
-        $frameResponseObject->addWidget($auswertungsPlatform);
+        $summaryPlatform = new \Widgets\RawHtml();
+        $summaryPlatform->setHtml('<div class="attribute">Auswertungs Plattform:</div><div class="value"><a target="_blank" href="'.$this->ellenbergUrl.'summary/'.$object->get_Attribute("ELLENBERG_ID").'/'.$userLoginName.'" > '.$this->ellenbergUrl.'summary/'.$object->get_Attribute("ELLENBERG_ID").'/'.$userLoginName.'</a></div>');
+        $frameResponseObject->addWidget($summaryPlatform);
         
         $hintLocal = new \Widgets\RawHtml();
         $hintLocal->setHtml('<br /><br /><p class ="breadcrumb">Mit dem folgenden Link werden Sie zum Speicherort der Dateien in diesem System weitergeleitet.</p>');
