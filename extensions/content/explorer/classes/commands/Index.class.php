b1d856f4e326b57629eeed8edb88959b50a19702
Andreas Schultz <sourcetree@haferfeld.de>
2015-03-17 14:59:34 +0100

    added an catch statement

--- extensions/content/explorer/classes/commands/Index.class.php
+++ extensions/content/explorer/classes/commands/Index.class.php
@@ -96,6 +96,8 @@ class Index extends \AbstractCommand implements \IFrameCommand {
             $objects = $object->get_inventory();
             
             }catch(NotFoundException $e) {\ExtensionMaster::getInstance()->send404Error();}
+            catch (\AccessDeniedException $e) {throw new \Exception("", E_USER_ACCESS_DENIED);}
+            
         } else {
             $objects = array();
         }
