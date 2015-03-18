5aaf5a809f56014faaeaba14a8ddd20bfbf1a3d4
Marcel Jakoblew <mjako@upb.de>
2015-03-18 14:47:55 +0100

    fix not logged in case 2

--- extensions/system/application/classes/Application.extension.php
+++ extensions/system/application/classes/Application.extension.php
@@ -70,7 +70,9 @@ class Application extends AbstractExtension implements IApplicationExtension, II
 			if ($command->httpAuth($urlRequestObject)) {
 				include_once PATH_LIB . "http_auth_handling.inc.php";
 				if (!http_auth()) {
-					throw new \Exception("Bitte anmelden.");
+                                        //not logged in case
+					//throw new \Exception("Bitte anmelden.");
+                                        throw new Exception("", E_USER_ACCESS_DENIED);
 				}
 			}
 			
