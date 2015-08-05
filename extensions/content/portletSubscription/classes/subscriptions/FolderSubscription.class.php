8c9f9f37962aef0a4323e05882b62eb73513ce7a
Marcel Jakoblew <mjako@upb.de>
2015-08-05 18:22:23 +0200

    fix

--- extensions/content/portletSubscription/classes/subscriptions/FolderSubscription.class.php
+++ extensions/content/portletSubscription/classes/subscriptions/FolderSubscription.class.php
@@ -45,7 +45,7 @@ class FolderSubscription extends AbstractSubscription {
                                         $object->get_id() . "_" . $count,
                                         $this->private,
                                         $object->get_attribute("OBJ_LAST_CHANGED"),
-                                        "Geänderter Ordner1:",
+                                        "Geändertes Objekt:",
                                         \PortletSubscription::getNameForSubscription($object),
                                         \ExtensionMaster::getInstance()->getUrlForObjectId($object->get_id(), "view")
                                     )
@@ -63,7 +63,7 @@ class FolderSubscription extends AbstractSubscription {
                                         $object->get_id() . "_" . $count,
                                         $this->private,
                                         $object->get_attribute("CONT_LAST_MODIFIED"),
-                                        "Geänderter Ordner2:",
+                                        "Geänderter Ordner:",
                                         \PortletSubscription::getNameForSubscription($object),
                                         \ExtensionMaster::getInstance()->getUrlForObjectId($object->get_id(), "view")
                                     )
