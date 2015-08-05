4f0d30b26273b7dd35214cfa11fcbc153b444947
Andreas Schultz <sourcetree@haferfeld.de>
2015-08-05 14:02:17 +0200

    improved output for debugging

--- extensions/content/sanction/classes/commands/Index.class.php
+++ extensions/content/sanction/classes/commands/Index.class.php
@@ -442,7 +442,10 @@ class Index extends \AbstractCommand implements \IFrameCommand {
                 $content->setVariable("VALUE", "<pre>".var_export($value, true)."</pre>");
             }
             else if(is_object($value)){
-                $content->setVariable("VALUE", "Objekt mit der Id ".$value);
+                $content->setVariable("VALUE", "Objekt mit der Id ".$value. " und dem Namen \"". $value->get_name()."\"");
+            }
+            else if(in_array($key, array("CONT_LAST_MODIFIED", "OBJ_CREATION_TIME", "OBJ_LAST_CHANGED", "PORTLET_SUBSCRIPTION_TIMESTAMP"))){
+                $content->setVariable("VALUE", $value ." (".date("d.m.Y H:i", $value).")");
             }
             else {
                 $content->setVariable("VALUE", $value);
