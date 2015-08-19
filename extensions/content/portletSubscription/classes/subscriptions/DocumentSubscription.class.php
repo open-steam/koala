ad344df73bb9db7c420b5f5269f94b2934d9375e
Andreas Schultz <sourcetree@haferfeld.de>
2015-08-19 18:46:42 +0200

    prevent showing duplicated entries with documents

--- extensions/content/portletSubscription/classes/subscriptions/DocumentSubscription.class.php
+++ extensions/content/portletSubscription/classes/subscriptions/DocumentSubscription.class.php
@@ -6,7 +6,20 @@ class DocumentSubscription extends AbstractSubscription {
     public function getUpdates() {
         $updates = array();
         $document = $this->object;
+       
         if ($document->get_attribute("DOC_LAST_MODIFIED") > $this->timestamp && !(isset($this->filter[$document->get_id()]) && in_array($document->get_attribute("DOC_LAST_MODIFIED"), $this->filter[$document->get_id()]))) {
+            
+            //scan the existing updates for an eintry with this object id and if it exists, don't show another notifyevent
+            $alreadyContained=false;
+            foreach($updates as $key1 => $value1) { 
+		if($value[1] == $document->get_id()){ 
+                    $alreadyContained=true;
+                    break;
+                } 
+            } 
+            if($alreadyContained) return $updates;
+            
+            //else show this change
             $updates[] = array(
                             $document->get_attribute("DOC_LAST_MODIFIED"), 
                             $document->get_id(), 
