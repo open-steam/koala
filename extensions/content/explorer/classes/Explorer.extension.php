dcfd8fff3986dc39451ac121f2140177d0028000
Steven Luecker <sluecker@mail.upb.de>
2016-01-10 22:52:15 +0100

    Explorer bugfix

--- extensions/content/explorer/classes/Explorer.extension.php
+++ extensions/content/explorer/classes/Explorer.extension.php
@@ -1,7 +1,7 @@
 <?php
 
 class Explorer extends AbstractExtension implements IIconBarExtension {
-    
+
     public function getName() {
         return "Explorer";
     }
@@ -29,16 +29,17 @@ class Explorer extends AbstractExtension implements IIconBarExtension {
         $trashbin = $currentUser->get_attribute(USER_TRASHBIN);
         $trashbinModel = new \Explorer\Model\Trashbin($trashbin);
         $trashbinCount = count($trashbin->get_inventory());
-        
+
         $showTrashbin = $trashbinCount == 0 ? FALSE : TRUE;
         $clipboardModel = new \Explorer\Model\Clipboard($currentUser);
         $clipboardCount = count($currentUser->get_inventory());
         $showClipboard = $clipboardCount == 0 ? FALSE : TRUE;
         $array = array();
         $path = strtolower($_SERVER["REQUEST_URI"]);
-        
-        $arr = explode('/', $path);
-        $id = $arr[count($arr)-2];
+
+        $pathShort = rtrim($path, "/");
+        $arr = explode('/', $pathShort);
+        $id = $arr[count($arr)-1];
         /*
         $pos = strpos($path, "explorer");
         if ($pos !== false) {
@@ -48,15 +49,15 @@ class Explorer extends AbstractExtension implements IIconBarExtension {
             $envSanction = $object->check_access(SANCTION_SANCTION);
         }
          * */
-        
+
         if ($path == "/explorer/") {
             $array[] = array("name" => "<img id=\"sort-icon\" name=\"false\" onclick=\"if(name == 'false'){initSort();}else{window.location.reload();}\" title=\"Sortieren\" src=\"" . \Portal::getInstance()->getAssetUrl() . "icons/portal_sort_white.png\">");
             $array[] = array("name" => "<img name=\"false\" title=\"Navigationsbaum\" src=\"" . \FileTree::getInstance()->getAssetUrl() . "icons/tree_white.png\">","onclick" => "openFileTree()");
-        
+
             $object = $currentUser->get_workroom();
             $envWriteable = ($object->check_access_write($GLOBALS["STEAM"]->get_current_steam_user()));
             $envSanction = $object->check_access(SANCTION_SANCTION);
-            
+
             if ($envSanction) {
                     $array[] = array("name" => "<img title=\"Neu\" src=\"" . \Explorer::getInstance()->getAssetUrl() . "icons/menu/newElement_white.png\">", "onclick"=>"sendRequest('newElement', {'id':{$object->get_id()}}, '', 'popup', null, null, 'explorer');return false;");
                     $array[] = array("name" => "<img title=\"Eigenschaften\" src=\"" . \Explorer::getInstance()->getAssetUrl() . "icons/menu/properties_white.png\">", "onclick"=>"sendRequest('Properties', {'id':{$object->get_id()}}, '', 'popup', null, null, 'explorer');return false;");
@@ -67,15 +68,15 @@ class Explorer extends AbstractExtension implements IIconBarExtension {
             } else {
                     $array[] = array("name" => "<img title=\"Eigenschaften\" src=\"" . \Explorer::getInstance()->getAssetUrl() . "icons/menu/properties_white.png\">", "onclick"=>"sendRequest('Properties', {'id':{$object->get_id()}}, '', 'popup', null, null, 'explorer');return false;");
             }
-            
+
         } else if (strpos($path, "/explorer/index/") !== false) {
             $array[] = array("name" => "<img id=\"sort-icon\" name=\"false\" onclick=\"if(name == 'false'){initSort();}else{window.location.reload();}\" title=\"Sortieren\" src=\"" . \Portal::getInstance()->getAssetUrl() . "icons/portal_sort_white.png\">");
             $array[] = array("name" => "<img name=\"false\" title=\"Navigationsbaum\" src=\"" . \FileTree::getInstance()->getAssetUrl() . "icons/tree_white.png\">","onclick" => "openFileTree()");
-        
+
             $object = \steam_factory::get_object($GLOBALS["STEAM"]->get_id(), $id);
             $envWriteable = ($object->check_access_write($GLOBALS["STEAM"]->get_current_steam_user()));
             $envSanction = $object->check_access(SANCTION_SANCTION);
-            
+
             if ($envSanction) {
                     $array[] = array("name" => "<img title=\"Neu\" src=\"" . \Explorer::getInstance()->getAssetUrl() . "icons/menu/newElement_white.png\">", "onclick"=>"sendRequest('newElement', {'id':{$object->get_id()}}, '', 'popup', null, null, 'explorer');return false;");
                     $array[] = array("name" => "<img title=\"Eigenschaften\" src=\"" . \Explorer::getInstance()->getAssetUrl() . "icons/menu/properties_white.png\">", "onclick"=>"sendRequest('Properties', {'id':{$object->get_id()}}, '', 'popup', null, null, 'explorer');return false;");
@@ -86,7 +87,7 @@ class Explorer extends AbstractExtension implements IIconBarExtension {
             } else {
                     $array[] = array("name" => "<img title=\"Eigenschaften\" src=\"" . \Explorer::getInstance()->getAssetUrl() . "icons/menu/properties_white.png\">", "onclick"=>"sendRequest('Properties', {'id':{$object->get_id()}}, '', 'popup', null, null, 'explorer');return false;");
             }
-            
+
         } else if ($path == "/bookmarks/") {
             $object = $currentUser->get_attribute("USER_BOOKMARKROOM");
             $array[] = array("name" => "<img id=\"sort-icon\" name=\"false\" onclick=\"if(name == 'false'){initSort();}else{window.location.reload();}\" title=\"Sortieren\" src=\"" . \Portal::getInstance()->getAssetUrl() . "icons/portal_sort_white.png\">");
@@ -95,7 +96,7 @@ class Explorer extends AbstractExtension implements IIconBarExtension {
             $array[] = array("name" => "<img id=\"sort-icon\" name=\"false\" onclick=\"if(name == 'false'){initSort();}else{window.location.reload();}\" title=\"Sortieren\" src=\"" . \Portal::getInstance()->getAssetUrl() . "icons/portal_sort_white.png\">");
             $array[] = array("name" => "<img title=\"Ordner anlegen\" src=\"" . \Explorer::getInstance()->getAssetUrl() . "icons/menu/newFolder_white.png\">", "onclick"=>"sendRequest('newElement', {'id':{$id}}, '', 'popup', null, null, 'Bookmarks');return false;");
         }
-     
+
         if ($showClipboard) {
             $array[] = array("name" => "<div id=\"clipboardIconbarWrapper\">" . $clipboardModel->getIconbarHtml() . "</div>",
                 "menu" => array(
@@ -107,7 +108,7 @@ class Explorer extends AbstractExtension implements IIconBarExtension {
                 "menu" => array(
                     array("name" => "Zwischenablage öffnen", "link" => "/clipboard/")));
         }
-        
+
         if ($showTrashbin) {
             $array[] = array("name" => "<div id=\"trashbinIconbarWrapper\">" . $trashbinModel->getIconbarHtml() . "</div>",
                 "menu" => array(
