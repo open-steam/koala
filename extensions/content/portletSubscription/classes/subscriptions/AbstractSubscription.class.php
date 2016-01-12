32dc350d156e863aa682a1810a1153957f87bd66
Steven Luecker <sluecker@mail.upb.de>
2016-01-12 11:36:44 +0100

    Wiki & Subscription: class name fix

--- extensions/content/portletSubscription/classes/subscriptions/AbstractSubscription.class.php
+++ extensions/content/portletSubscription/classes/subscriptions/AbstractSubscription.class.php
@@ -2,14 +2,14 @@
 namespace PortletSubscription\Subscriptions;
 
 abstract class AbstractSubscription {
-    
+
     protected $portlet;
     protected $object;
     protected $private;
     protected $timestamp;
     protected $filter;
     protected $depth;
-    
+
     public function __construct($portlet, $object, $private, $timestamp, $filter, $depth) {
         $this->portlet = $portlet;
         $this->object = $object;
@@ -18,16 +18,16 @@ abstract class AbstractSubscription {
         $this->filter = $filter;
         $this->depth = $depth;
     }
-    
+
     abstract public function getUpdates();
-    
+
     protected function getElementHtml($id, $divid, $private, $timestamp, $text, $linktext, $link, $additionalHTML = "") {
-        
+
         //for deleted item it is not possible to give a exact date.
-        if(date("Y", $timestamp)){ 
+        if(date("Y", $timestamp)){
             //try to build a date out of the timestamp, then build the correct formatted date
             $dateString = date("d.m.Y H:i", $timestamp) . " Uhr";
-        } else { 
+        } else {
             //if it fails return a general phrase
             $dateString = "In letzter Zeit";
             $timestamp = -1;
@@ -37,9 +37,9 @@ abstract class AbstractSubscription {
             <h2 class=\"subheadline\">" . $dateString;
         if ($private === TRUE) {
             $params = "{ id : " . $this->portlet->get_id() . ", objectID : " . $id . ", timestamp : " . $timestamp . ", hide : 'subscription" . $this->portlet->get_id() . "_" . $divid . "' }";
-            $html .= "<div class=\"close-button\" title=\"ausblenden\" onclick=\"sendRequest('HideItem', " . $params . ", 'subscription" . $this->portlet->get_id() . "_" . $divid . "', 'updater', '', '', 'PortletSubscription');\" style=\"float:right;\"></div>";
+            $html .= "<div class=\"subscription-close-button\" title=\"ausblenden\" onclick=\"sendRequest('HideItem', " . $params . ", 'subscription" . $this->portlet->get_id() . "_" . $divid . "', 'updater', '', '', 'PortletSubscription');\" style=\"float:right;\"></div>";
         }
-        
+
         $html .= "</h2>
             <h3>" . $text . " <a href=\"" . $link . "\">" . $linktext . "</a>" . $additionalHTML . "</h3>
             </div>";
