32d3f1b96da33ea2780faf70a0e9894b6be671c0
Steven Luecker <sluecker@mail.upb.de>
2016-02-25 14:27:48 +0100

    Portlet Messages: Only display possible sort options

--- extensions/content/portletMsg/classes/commands/GetPopupMenuMessage.class.php
+++ extensions/content/portletMsg/classes/commands/GetPopupMenuMessage.class.php
@@ -1,34 +1,29 @@
 <?php
 namespace PortletMsg\Commands;
 class GetPopupMenuMessage extends \AbstractCommand implements \IAjaxCommand {
-	
+
 	private $params;
-	private $object;
 	private $x, $y, $height, $width;
-	
 	private $portletObjectId;
 	private $messageObjectId;
-	private $id;
 	private $user;
-	
-	
+
 	public function validateData(\IRequestObject $requestObject) {
 		return true;
 	}
-	
-	public function processData(\IRequestObject $requestObject) {		
+
+	public function processData(\IRequestObject $requestObject) {
 		$this->params = $requestObject->getParams();
-		
+
 		$this->x = $this->params["x"];
 		$this->y = $this->params["y"];
 		$this->height = $this->params["height"];
 		$this->width = $this->params["width"];
 		$this->user = $GLOBALS["STEAM"]->get_current_steam_user()->get_name();
-		
 		$this->messageObjectId = $this->params["messageObjectId"];
 		$this->portletObjectId = $this->params["portletObjectId"];
 	}
-	
+
 	public function ajaxResponse(\AjaxResponseObject $ajaxResponseObject) {
 		//icons
 		$copyIcon = \Explorer::getInstance()->getAssetUrl() . "icons/menu/copy.png";
@@ -46,24 +41,43 @@ class GetPopupMenuMessage extends \AbstractCommand implements \IAjaxCommand {
 		$propertiesIcon = \Explorer::getInstance()->getAssetUrl() . "icons/menu/properties.png";
 		$rightsIcon = \Explorer::getInstance()->getAssetUrl() . "icons/menu/rights.png";
 		$blankIcon = \Explorer::getInstance()->getAssetUrl() . "icons/menu/blank.png";
-                $addImage = \PortletMsg::getInstance()->getAssetUrl() . "icons/add_image.png";
-		
+		$addImage = \PortletMsg::getInstance()->getAssetUrl() . "icons/add_image.png";
+
+		$messageObject = \steam_factory::get_object($GLOBALS["STEAM"]->get_id(), $this->messageObjectId);
+		$portletObject = \steam_factory::get_object($GLOBALS["STEAM"]->get_id(), $this->portletObjectId);
+
+		$content = $portletObject->get_attribute("bid:portlet:content");
+		$messageId = intval($this->messageObjectId);
+		foreach ($content as $key => $id) {
+			if ($id == $messageId) {
+				$index = $key;
+			}
+		}
+
+		$imageId = $messageObject->get_attribute("bid:portlet:msg:picture_id");
+		if ($imageId !== 0){
+			$pictureLabel = "Bild bearbeiten";
+		} else{
+			$pictureLabel = "Bild hinzufügen";
+		}
+
 		$popupMenu =  new \Widgets\PopupMenu();
 		$items = array(	array("name" => "Bearbeiten <img src=\"{$editIcon}\">",  "command" => "EditMessage", "namespace" => "PortletMsg", "params" => "{'portletObjectId':'{$this->portletObjectId}','messageObjectId':'{$this->messageObjectId}'}", "type"=>"popup"),
-                                                array("name" => "Quelltext bearbeiten <img src=\"{$editIcon}\">",  "command" => "EditMessageCode", "namespace" => "PortletMsg", "params" => "{'portletObjectId':'{$this->portletObjectId}','messageObjectId':'{$this->messageObjectId}'}", "type"=>"popup"),
-						array("name" => "Bild bearbeiten <img src=\"{$addImage}\">",  "command" => "EditMessageImage", "namespace" => "PortletMsg", "params" => "{'portletObjectId':'{$this->portletObjectId}','messageObjectId':'{$this->messageObjectId}'}", "type"=>"popup"), 
-						array("name" => "Umsortieren <img src=\"{$blankIcon}\">", "direction" => "left", "menu" => array(
-							array("name" => "Eins nach oben <img src=\"{$upIcon}\">",  "command" => "OrderMessage", "namespace" => "PortletMsg", "params" => "{'portletObjectId':'{$this->portletObjectId}','messageObjectId':'{$this->messageObjectId}','order':'up'}", "type"=>"popup"),
-							array("name" => "Eins nach unten <img src=\"{$downIcon}\">",  "command" => "OrderMessage", "namespace" => "PortletMsg", "params" => "{'portletObjectId':'{$this->portletObjectId}','messageObjectId':'{$this->messageObjectId}','order':'down'}", "type"=>"popup"),
-							array("name" => "Ganz nach oben <img src=\"{$topIcon}\">",  "command" => "OrderMessage", "namespace" => "PortletMsg", "params" => "{'portletObjectId':'{$this->portletObjectId}','messageObjectId':'{$this->messageObjectId}','order':'first'}", "type"=>"popup"),
-							array("name" => "Ganz nach unten <img src=\"{$bottomIcon}\">",  "command" => "OrderMessage", "namespace" => "PortletMsg", "params" => "{'portletObjectId':'{$this->portletObjectId}','messageObjectId':'{$this->messageObjectId}','order':'last'}", "type"=>"popup")
-						)),
-						array("name" => "Löschen <img src=\"{$deleteIcon}\">",  "command" => "DeleteMessage", "namespace" => "PortletMsg", "params" => "{'portletObjectId':'{$this->portletObjectId}','messageObjectId':'{$this->messageObjectId}'}", "type"=>"popup"),
-						);
+										array("name" => "Quelltext bearbeiten <img src=\"{$editIcon}\">",  "command" => "EditMessageCode", "namespace" => "PortletMsg", "params" => "{'portletObjectId':'{$this->portletObjectId}','messageObjectId':'{$this->messageObjectId}'}", "type"=>"popup"),
+										array("name" => "{$pictureLabel} <img src=\"{$addImage}\">",  "command" => "EditMessageImage", "namespace" => "PortletMsg", "params" => "{'portletObjectId':'{$this->portletObjectId}','messageObjectId':'{$this->messageObjectId}'}", "type"=>"popup"),
+										(count($content) > 1) ? array("name" => "Umsortieren <img src=\"{$blankIcon}\">", "direction" => "left", "menu" => array(
+											($index != 0) ? array("name" => "Ganz nach oben <img src=\"{$topIcon}\">",  "command" => "OrderMessage", "namespace" => "PortletMsg", "params" => "{'portletObjectId':'{$this->portletObjectId}','messageObjectId':'{$this->messageObjectId}','order':'first'}") : "",
+											($index != 0) ? array("name" => "Eins nach oben <img src=\"{$upIcon}\">",  "command" => "OrderMessage", "namespace" => "PortletMsg", "params" => "{'portletObjectId':'{$this->portletObjectId}','messageObjectId':'{$this->messageObjectId}','order':'up'}") : "",
+											($index < count($content)-1) ? array("name" => "Eins nach unten <img src=\"{$downIcon}\">",  "command" => "OrderMessage", "namespace" => "PortletMsg", "params" => "{'portletObjectId':'{$this->portletObjectId}','messageObjectId':'{$this->messageObjectId}','order':'down'}") : "",
+											($index < count($content)-1) ? array("name" => "Ganz nach unten <img src=\"{$bottomIcon}\">",  "command" => "OrderMessage", "namespace" => "PortletMsg", "params" => "{'portletObjectId':'{$this->portletObjectId}','messageObjectId':'{$this->messageObjectId}','order':'last'}") : ""
+										)
+									) : "",
+									array("name" => "Löschen <img src=\"{$deleteIcon}\">",  "command" => "DeleteMessage", "namespace" => "PortletMsg", "params" => "{'portletObjectId':'{$this->portletObjectId}','messageObjectId':'{$this->messageObjectId}'}", "type"=>"popup"),
+								);
 		$popupMenu->setItems($items);
 		$popupMenu->setPosition(round($this->x + $this->width - 155) . "px", round($this->y + $this->height + 4) . "px");
 		$popupMenu->setWidth("180px");
-		
+
 		$ajaxResponseObject->setStatus("ok");
 		$ajaxResponseObject->addWidget($popupMenu);
 		return $ajaxResponseObject;
