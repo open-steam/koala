3d17cd8eecc95cc102678ae604b1550254104514
Steven Luecker <sluecker@mail.upb.de>
2016-02-29 11:07:01 +0100

    Portlet Termplan: Only display possible sort options

--- extensions/content/portletTermplan/classes/commands/GetPopupMenu.class.php
+++ extensions/content/portletTermplan/classes/commands/GetPopupMenu.class.php
@@ -1,18 +1,18 @@
 <?php
 namespace PortletTermplan\Commands;
 class GetPopupMenu extends \AbstractCommand implements \IAjaxCommand {
-	
+
 	private $params;
 	private $id;
 	private $object;
 	private $x, $y, $height, $width;
 	private $user;
-	
+
 	public function validateData(\IRequestObject $requestObject) {
 		return true;
 	}
-	
-	public function processData(\IRequestObject $requestObject) {		
+
+	public function processData(\IRequestObject $requestObject) {
 		$this->params = $requestObject->getParams();
 		$this->id = $this->params["id"];
 		$this->x = $this->params["x"];
@@ -20,10 +20,9 @@ class GetPopupMenu extends \AbstractCommand implements \IAjaxCommand {
 		$this->height = $this->params["height"];
 		$this->width = $this->params["width"];
 		$this->user = $GLOBALS["STEAM"]->get_current_steam_user()->get_name();
-		
 		$this->object = \steam_factory::get_object($GLOBALS["STEAM"]->get_id(), $this->id);
 	}
-	
+
 	public function ajaxResponse(\AjaxResponseObject $ajaxResponseObject) {
 		//icons
 		$copyIcon = \Explorer::getInstance()->getAssetUrl() . "icons/menu/copy.png";
@@ -41,19 +40,27 @@ class GetPopupMenu extends \AbstractCommand implements \IAjaxCommand {
 		$propertiesIcon = \Explorer::getInstance()->getAssetUrl() . "icons/menu/properties.png";
 		$rightsIcon = \Explorer::getInstance()->getAssetUrl() . "icons/menu/rights.png";
 		$blankIcon = \Explorer::getInstance()->getAssetUrl() . "icons/menu/blank.png";
-		
+
+		$env = $this->object->get_environment();
+		$inventory = $env->get_inventory();
+		$id = intval($this->id);
+		foreach ($inventory as $key => $element) {
+			if ($element->get_id() == $id) {
+				$index = $key;
+			}
+		}
 		
 		$popupMenu =  new \Widgets\PopupMenu();
 		$items = array(	array("name" => "Bearbeiten <img src=\"{$editIcon}\">",  "command" => "Edit", "namespace" => "PortletTermplan", "params" => "{'portletId':'{$this->id}'}", "type"=>"popup"),
-						array("name" => "Umsortieren <img src=\"{$blankIcon}\">", "direction" => "left", "menu" => array(
-							array("name" => "Eins nach oben <img src=\"{$upIcon}\">",  "command" => "Order", "namespace" => "Portal", "params" => "{'portletId':'{$this->id}','order':'up'}", "type"=>"popup"),
-							array("name" => "Eins nach unten <img src=\"{$downIcon}\">",  "command" => "Order", "namespace" => "Portal", "params" => "{'portletId':'{$this->id}','order':'down'}", "type"=>"popup"),
-							array("name" => "Ganz nach oben <img src=\"{$topIcon}\">",  "command" => "Order", "namespace" => "Portal", "params" => "{'portletId':'{$this->id}','order':'first'}", "type"=>"popup"),
-							array("name" => "Ganz nach unten <img src=\"{$bottomIcon}\">",  "command" => "Order", "namespace" => "Portal", "params" => "{'portletId':'{$this->id}','order':'last'}", "type"=>"popup")
-						)),
+						(count($inventory) > 1) ? array("name" => "Umsortieren <img src=\"{$blankIcon}\">", "direction" => "left", "menu" => array(
+							($index != 0) ? array("name" => "Ganz nach oben <img src=\"{$topIcon}\">",  "command" => "Order", "namespace" => "Portal", "params" => "{'portletId':'{$this->id}','order':'first'}") : "",
+							($index != 0) ? array("name" => "Eins nach oben <img src=\"{$upIcon}\">",  "command" => "Order", "namespace" => "Portal", "params" => "{'portletId':'{$this->id}','order':'up'}") : "",
+							($index < count($inventory)-1) ? array("name" => "Eins nach unten <img src=\"{$downIcon}\">",  "command" => "Order", "namespace" => "Portal", "params" => "{'portletId':'{$this->id}','order':'down'}") : "",
+							($index < count($inventory)-1) ? array("name" => "Ganz nach unten <img src=\"{$bottomIcon}\">",  "command" => "Order", "namespace" => "Portal", "params" => "{'portletId':'{$this->id}','order':'last'}") : "",
+						)) : "",
 						array("name" => "SEPARATOR"),
-						array("name" => "Kopieren <img src=\"{$copyIcon}\">",  "command" => "PortletCopy", "namespace" => "Portal", "params" => "{'id':'{$this->id}','user':'{$this->user}'}", "type"=>"popup"), 
-						array("name" => "Ausschneiden <img src=\"{$cutIcon}\">",  "command" => "PortletCut", "namespace" => "Portal", "params" => "{'id':'{$this->id}','user':'{$this->user}'}", "type"=>"popup"), 
+						array("name" => "Kopieren <img src=\"{$copyIcon}\">",  "command" => "PortletCopy", "namespace" => "Portal", "params" => "{'id':'{$this->id}','user':'{$this->user}'}", "type"=>"popup"),
+						array("name" => "Ausschneiden <img src=\"{$cutIcon}\">",  "command" => "PortletCut", "namespace" => "Portal", "params" => "{'id':'{$this->id}','user':'{$this->user}'}", "type"=>"popup"),
 						array("name" => "Referenzieren <img src=\"{$referIcon}\">",  "command" => "PortletReference", "namespace" => "Portal", "params" => "{'id':'{$this->id}','user':'{$this->user}'}", "type"=>"popup"),
 						array("name" => "Löschen <img src=\"{$trashIcon}\">",  "command" => "Delete", "namespace" => "PortletTermplan", "params" => "{'portletId':'{$this->id}'}", "type"=>"popup"),
 						array("name" => "SEPARATOR"),
@@ -62,7 +69,7 @@ class GetPopupMenu extends \AbstractCommand implements \IAjaxCommand {
 		$popupMenu->setItems($items);
 		$popupMenu->setPosition(round($this->x + $this->width - 155) . "px", round($this->y + $this->height + 4) . "px");
 		$popupMenu->setWidth("150px");
-		
+
 		$ajaxResponseObject->setStatus("ok");
 		$ajaxResponseObject->addWidget($popupMenu);
 		return $ajaxResponseObject;
