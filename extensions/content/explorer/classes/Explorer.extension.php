a84bd16d04e393b49a5e483ded1ffdcfd1bbcb93
Steven Luecker <sluecker@mail.upb.de>
2016-03-02 16:09:30 +0100

    Bookmarks: Change create folder icon

--- extensions/content/explorer/classes/Explorer.extension.php
+++ extensions/content/explorer/classes/Explorer.extension.php
@@ -140,7 +140,7 @@ class Explorer extends AbstractExtension implements IIconBarExtension {
         } else if ($path == "/bookmarks/") {
             $object = $currentUser->get_attribute("USER_BOOKMARKROOM");
             $array[] = array("name" => "<img id=\"sort-icon\" name=\"false\" onclick=\"if(name == 'false'){initSort();}else{window.location.reload();}\" title=\"Sortieren\" src=\"" . \Portal::getInstance()->getAssetUrl() . "icons/explorer_sort_white.png\">");
-            $array[] = array("name" => "<img title=\"Ordner anlegen\" src=\"" . \Explorer::getInstance()->getAssetUrl() . "icons/menu/newFolder_white.png\">", "onclick"=>"sendRequest('newElement', {'id':{$object->get_id()}}, '', 'popup', null, null, 'Bookmarks');return false;");
+            $array[] = array("name" => "<img title=\"Ordner anlegen\" src=\"" . \Explorer::getInstance()->getAssetUrl() . "icons/menu/folder_white.png\">", "onclick"=>"sendRequest('newElement', {'id':{$object->get_id()}}, '', 'popup', null, null, 'Bookmarks');return false;");
         } else if (strpos($path, "/bookmarks/index/") !== false) {
             $array[] = array("name" => "<img id=\"sort-icon\" name=\"false\" onclick=\"if(name == 'false'){initSort();}else{window.location.reload();}\" title=\"Sortieren\" src=\"" . \Portal::getInstance()->getAssetUrl() . "icons/explorer_sort_white.png\">");
             if($currentObjectID != ""){
