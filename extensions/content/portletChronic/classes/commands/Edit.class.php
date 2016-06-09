057ce9b1294358d9d68c47e58a85a7ec0f087a13
Andreas Schultz <sourcetree@haferfeld.de>
2016-06-09 11:51:44 +0200

    prevent negative numbers in chronic portlet

--- extensions/content/portletChronic/classes/commands/Edit.class.php
+++ extensions/content/portletChronic/classes/commands/Edit.class.php
@@ -1,40 +1,44 @@
 <?php
+
 namespace PortletChronic\Commands;
 
 class Edit extends \AbstractCommand implements \IAjaxCommand {
 
-	private $dialog;
+    private $dialog;
 
-	public function validateData(\IRequestObject $requestObject) {
-		return true;
-	}
+    public function validateData(\IRequestObject $requestObject) {
+        return true;
+    }
 
-	public function processData(\IRequestObject $requestObject){
-		$params = $requestObject->getParams();
-		$objectId = $params["portletId"];
+    public function processData(\IRequestObject $requestObject) {
+        $params = $requestObject->getParams();
+        $objectId = $params["portletId"];
 
-		$clearer = new \Widgets\Clearer();
+        $clearer = new \Widgets\Clearer();
 
-		$object = \steam_factory::get_object($GLOBALS["STEAM"]->get_id(), $objectId);
-		$dialog = new \Widgets\Dialog();
-		$dialog->setTitle("Bearbeiten von " . $object->get_attribute("OBJ_DESC"));
+        $object = \steam_factory::get_object($GLOBALS["STEAM"]->get_id(), $objectId);
+        $dialog = new \Widgets\Dialog();
+        $dialog->setTitle("Bearbeiten von " . $object->get_attribute("OBJ_DESC"));
 
-		$numberInput = new \Widgets\TextInput();
-		$numberInput->setLabel("Sichtbare Objekte");
-		$numberInput->setData($object);
-		$numberInput->setType("number");
-		$numberInput->setContentProvider(\Widgets\DataProvider::attributeProvider("PORTLET_CHRONIC_COUNT"));
+        $numberInput = new \Widgets\TextInput();
+        $numberInput->setLabel("Sichtbare Objekte");
+        $numberInput->setData($object);
+        $numberInput->setType("number");
+        $numberInput->setMin(1);
+        $numberInput->setContentProvider(\Widgets\DataProvider::attributeProvider("PORTLET_CHRONIC_COUNT"));
 
-    $dialog->addWidget($numberInput);
-    $dialog->addWidget($clearer);
+        $dialog->addWidget($numberInput);
+        $dialog->addWidget($clearer);
 
-		$this->dialog = $dialog;
-	}
+        $this->dialog = $dialog;
+    }
 
-	public function ajaxResponse(\AjaxResponseObject $ajaxResponseObject) {
-		$ajaxResponseObject->setStatus("ok");
-		$ajaxResponseObject->addWidget($this->dialog);
-		return $ajaxResponseObject;
-	}
+    public function ajaxResponse(\AjaxResponseObject $ajaxResponseObject) {
+        $ajaxResponseObject->setStatus("ok");
+        $ajaxResponseObject->addWidget($this->dialog);
+        return $ajaxResponseObject;
+    }
+
 }
+
 ?>
