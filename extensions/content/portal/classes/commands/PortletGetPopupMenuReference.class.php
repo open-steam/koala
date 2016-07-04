dcf0a0f60df13a5a9cdf99005c532d310583fc23
Steven Luecker <sluecker@mail.upb.de>
2016-07-04 10:57:07 +0200

    Portlet Reference: Edit popup menu

--- extensions/content/portal/classes/commands/PortletGetPopupMenuReference.class.php
+++ extensions/content/portal/classes/commands/PortletGetPopupMenuReference.class.php
@@ -8,6 +8,7 @@ class PortletGetPopupMenuReference extends \AbstractCommand implements \IAjaxCom
 	private $sourceObjectId;
 	private $linkObjectId;
 	private $user;
+	private $column;
 
 	public function validateData(\IRequestObject $requestObject) {
 		return true;
@@ -24,8 +25,9 @@ class PortletGetPopupMenuReference extends \AbstractCommand implements \IAjaxCom
 		$this->sourceObjectId = $this->params["sourceObjectId"];
 		$this->linkObjectId = $this->params["linkObjectId"];
 
-		$portletObject = \steam_factory::get_object( $GLOBALS["STEAM"]->get_id(), $this->sourceObjectId );
-		$portal = $portletObject->get_environment()->get_environment();
+		$portletObject = \steam_factory::get_object($GLOBALS["STEAM"]->get_id(), $this->sourceObjectId);
+		$this->column = $portletObject->get_environment();
+		$portal = $this->column->get_environment();
 
 		$this->sourcePortalObjectId = $portal->get_id();
 		$this->user = $GLOBALS["STEAM"]->get_current_steam_user()->get_name();
@@ -33,29 +35,38 @@ class PortletGetPopupMenuReference extends \AbstractCommand implements \IAjaxCom
 
 	public function ajaxResponse(\AjaxResponseObject $ajaxResponseObject) {
 		//icons
-		$copyIcon = \Explorer::getInstance()->getAssetUrl() . "icons/menu/copy.png";
-		$cutIcon = \Explorer::getInstance()->getAssetUrl() . "icons/menu/cut.png";
-		$referIcon = \Explorer::getInstance()->getAssetUrl() . "icons/menu/refer.png";
-		$trashIcon = \Explorer::getInstance()->getAssetUrl() . "icons/menu/trash.png";
-		$upIcon = \Explorer::getInstance()->getAssetUrl() . "icons/menu/up.png";
-		$downIcon = \Explorer::getInstance()->getAssetUrl() . "icons/menu/down.png";
-		$topIcon = \Explorer::getInstance()->getAssetUrl() . "icons/menu/top.png";
-		$bottomIcon = \Explorer::getInstance()->getAssetUrl() . "icons/menu/bottom.png";
-		$rightsIcon = \Explorer::getInstance()->getAssetUrl() . "icons/menu/rights.png";
-		$blankIcon = \Explorer::getInstance()->getAssetUrl() . "icons/menu/blank.png";
+		$explorerUrl = \Explorer::getInstance()->getAssetUrl();
+		$copyIcon = $explorerUrl . "icons/menu/copy.png";
+		$cutIcon = $explorerUrl . "icons/menu/cut.png";
+		$referIcon = $explorerUrl . "icons/menu/refer.png";
+		$trashIcon = $explorerUrl . "icons/menu/trash.png";
+		$upIcon = $explorerUrl . "icons/menu/up.png";
+		$downIcon = $explorerUrl . "icons/menu/down.png";
+		$topIcon = $explorerUrl . "icons/menu/top.png";
+		$bottomIcon = $explorerUrl . "icons/menu/bottom.png";
+		$rightsIcon = $explorerUrl . "icons/menu/rights.png";
+		$blankIcon = $explorerUrl . "icons/menu/blank.png";
+		$sortIcon = $explorerUrl . "icons/menu/sort.png";
+
+		$inventory = $this->column->get_inventory();
+		$id = intval($this->linkObjectId);
+		foreach ($inventory as $key => $element) {
+			if ($element->get_id() == $id) {
+				$index = $key;
+			}
+		}
 
 		$popupMenu =  new \Widgets\PopupMenu();
 		$items = array(
-						array("name" => "Umsortieren<img src=\"{$blankIcon}\">", "direction" => "left", "menu" => array(
-							array("name" => "Eins nach oben<img src=\"{$upIcon}\">",  "command" => "Order", "namespace" => "Portal", "params" => "{'portletId':'{$this->linkObjectId}','order':'up'}", "type"=>"popup"),
-							array("name" => "Eins nach unten<img src=\"{$downIcon}\">",  "command" => "Order", "namespace" => "Portal", "params" => "{'portletId':'{$this->linkObjectId}','order':'down'}", "type"=>"popup"),
-							array("name" => "Ganz nach oben<img src=\"{$topIcon}\">",  "command" => "Order", "namespace" => "Portal", "params" => "{'portletId':'{$this->linkObjectId}','order':'first'}", "type"=>"popup"),
-							array("name" => "Ganz nach unten<img src=\"{$bottomIcon}\">",  "command" => "Order", "namespace" => "Portal", "params" => "{'portletId':'{$this->linkObjectId}','order':'last'}", "type"=>"popup"),
-						)),
+						(count($inventory) > 1) ? array("name" => "Umsortieren<img src=\"{$sortIcon}\">", "direction" => "left", "menu" => array(
+							($index != 0) ? array("name" => "Ganz nach oben<img src=\"{$topIcon}\">",  "command" => "Order", "namespace" => "Portal", "params" => "{'portletId':'{$this->linkObjectId}','order':'first'}") : "",
+							($index != 0) ? array("name" => "Eins nach oben<img src=\"{$upIcon}\">",  "command" => "Order", "namespace" => "Portal", "params" => "{'portletId':'{$this->linkObjectId}','order':'up'}") : "",
+							($index < count($inventory)-1) ? array("name" => "Eins nach unten<img src=\"{$downIcon}\">",  "command" => "Order", "namespace" => "Portal", "params" => "{'portletId':'{$this->linkObjectId}','order':'down'}") : "",
+							($index < count($inventory)-1) ? array("name" => "Ganz nach unten<img src=\"{$bottomIcon}\">",  "command" => "Order", "namespace" => "Portal", "params" => "{'portletId':'{$this->linkObjectId}','order':'last'}") : "",
+						)) : "",
 						array("name" => "SEPARATOR"),
 						array("name" => "Kopieren<img src=\"{$copyIcon}\">",  "command" => "PortletCopy", "namespace" => "Portal", "params" => "{'id':'{$this->linkObjectId}','user':'{$this->user}'}", "type"=>"popup"),
 						array("name" => "Ausschneiden<img src=\"{$cutIcon}\">",  "command" => "PortletCut", "namespace" => "Portal", "params" => "{'id':'{$this->linkObjectId}','user':'{$this->user}'}", "type"=>"popup"),
-						array("name" => "Referenz erstellen<img src=\"{$referIcon}\">",  "command" => "PortletReference", "namespace" => "Portal", "params" => "{'id':'{$this->sourceObjectId}','user':'{$this->user}'}", "type"=>"nonModalUpdater"),
 						array("name" => "Löschen<img src=\"{$trashIcon}\">",  "command" => "DeleteReference", "namespace" => "PortalColumn", "params" => "{'linkObjectId':'{$this->linkObjectId}'}", "type"=>"popup"),
 						array("name" => "SEPARATOR"),
 						array("name" => "Rechte<img src=\"{$rightsIcon}\">",  "command" => "Sanctions", "namespace" => "Explorer", "params" => "{'id':'{$this->linkObjectId}'}", "type"=>"popup"),
