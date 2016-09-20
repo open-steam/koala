7463fcbf83e04b606eb93fa158e7ed5a56f392ce
Steven Luecker <sluecker@mail.upb.de>
2016-09-20 14:20:38 +0200

    Make sure that the footer is always underneath the popupmenu

--- extensions/base/widgets/ui/js/PopupMenu.js
+++ extensions/base/widgets/ui/js/PopupMenu.js
@@ -1,4 +1,11 @@
 jQuery(document).click(function() {
   jQuery('.popupmenuwrapper').parent().html('');
   jQuery('.open').removeClass('open');
+  jQuery('#footer_wrapper').css('padding-top', '0px');
 })
+
+function adjustFooter() {
+  var popupBottom = jQuery('.popupmenuwrapper').position().top + jQuery('.popupmenuwrapper').height() + 20;
+  var footerTop = jQuery('#footer_wrapper').position().top;
+  if(popupBottom > footerTop) jQuery('#footer_wrapper').css('padding-top', popupBottom - footerTop + 'px');
+}
