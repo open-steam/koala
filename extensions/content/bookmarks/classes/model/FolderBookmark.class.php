6117ded48b04f660654125186d0c661fe7bb8ae8
Andreas Schultz <sourcetree@haferfeld.de>
2017-03-09 18:36:13 +0100

    no guest might be too hard

--- extensions/content/bookmarks/classes/model/FolderBookmark.class.php
+++ extensions/content/bookmarks/classes/model/FolderBookmark.class.php
@@ -3,8 +3,8 @@ namespace Bookmarks\Model;
 class FolderBookmark extends \AbstractObjectModel {
 		
 	public static function isObject(\steam_object $steamObject) {
-		$myuser = \lms_steam::get_current_user_no_guest();
-		if (is_object($steamObject) AND $myuser->get_attribute(USER_BOOKMARKROOM)->get_id() == $steamObject->get_id()) {
+		$myuser = \lms_steam::get_current_user();
+		if (is_object($steamObject) AND is_object($myuser) AND $myuser->get_attribute(USER_BOOKMARKROOM)->get_id() == $steamObject->get_id()) {
 			return true;
 		} else {
 			return false;
